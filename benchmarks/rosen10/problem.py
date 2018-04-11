26197990920dfab4d0bdb86329be98ece29959f5
msalim <msalim@anl.gov>
2018-04-11 00:31:22 +0000

    rosenbrock tests
    Former-commit-id: 48b3c81c887da5f036ce11ee111efec9b0c6b32e

---
+++ benchmarks/rosen10/problem.py
@@ -0,0 +1,15 @@
+from collections import OrderedDict
+import random
+NDIM = 10
+
+class Problem:
+    def __init__(self):
+        space = OrderedDict()
+
+        for i in range(1, 1+NDIM):
+            dim = f"x{i}"
+            space[dim] = (-3.0, 4.0)
+
+        self.space = space
+        self.params = self.space.keys()
+        self.starting_point = [random.uniform(-3.0, 4.0) for i in range(NDIM)]
