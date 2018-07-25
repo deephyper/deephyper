9449a0d5dbddac03d383c19fea9a6b93def9ad2b
Dipendra Jha <dipendra009@gmail.com>
2018-07-25 16:19:17 -0500

    update to PTM RNN loss and error
    Former-commit-id: f42be2f10943bf8b7c9b8d8a61b0129253dbdce6 [formerly 50434b0710037d69b627e20c074f19514235a861]
    Former-commit-id: ab2b257374c772ce38c6381925fa2239c7dbf6d6

--- model/builder/tf.py
+++ model/builder/tf.py
@@ -457,7 +457,7 @@ class RNNModel:
             self.eval_loss = loss_fun([self.eval_preds], [tf.reshape(self.eval_labels_node, [-1])],
                                  [tf.ones([self.eval_batch_size * self.num_steps])],
                                  self.vocab_size)
-            self.eval_loss = tf.reduce_sum(self.eval_loss) / (self.num_steps)
+            self.eval_loss = tf.reduce_sum(self.eval_loss) / (self.eval_batch_size * self.num_steps)
         else:
             self.loss = self.loss_metric(self.train_labels_node, self.logits)
             self.eval_loss = self.loss_metric(self.eval_labels_node, self.eval_preds)
