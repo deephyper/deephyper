c3df36b7928219f77ff0a7a159439a4b9e424f2d
Deathn0t <romainegele@gmail.com>
2018-10-02 16:38:08 -0500

    update for TC1
    Former-commit-id: 0e6cec5d407b08df09656d3274f56624b555a284 [formerly dd1c65950d7a04a4b3afefc14bb2fe0d28957b57]
    Former-commit-id: a2edd42d822d842fe42e92a5e5fe4a7f6af369e2

--- deephyper/benchmarks/candleTC1Nas/problem.py
+++ deephyper/benchmarks/candleTC1Nas/problem.py
@@ -1,42 +1,42 @@
-'''
- * @Author: romain.egele, dipendra.jha
- * @Date: 2018-06-21 15:31:30
+''' * @Author: romain.egele * @Date: 2018-06-21 15:31:30
 '''
 
 from collections import OrderedDict
-from deephyper.model.arch import StateSpace
+
+from nas.cell.structure import create_sequential_structure
+from nas.cell.mlp import create_dense_cell_type1
+from deephyper.benchmarks.candleTC1Nas.load_data import load_data
 
 class Problem:
     def __init__(self):
         space = OrderedDict()
-        space['num_outputs'] = 36
-        space['regression'] = False
+        space['regression'] = True
+        space['load_data'] = {
+            'func': load_data
+        }
 
         # ARCH
-        space['max_layers'] = 8
-        space['layer_type'] = 'conv1D'
-        state_space = StateSpace()
-        state_space.add_state('filter_size', [size for size in range(3, 10, 2)])
-        state_space.add_state('pool_size', [size for size in range(1, 6)])
-        state_space.add_state('stride_size', [s for s in range(1, 4)])
-        state_space.add_state('drop_out', [])
-        state_space.add_state('num_filters', [2 ** i for i in range(5, 10)])
-        state_space.add_state('skip_conn', [])
-        space['state_space'] = state_space
-
-        # ITER
-        space['max_episodes'] = 500 # iter on controller
+        space['create_structure'] = {
+            'func': create_sequential_structure,
+            'kwargs': {
+                'num_cells': 3
+            }
+        }
+        space['create_cell'] = {
+            'func': create_dense_cell_type1
+        }
 
         # HyperParameters
-        space['hyperparameters'] = {'batch_size': 64,
-                                    'eval_batch_size': 64,#needs to be same as batch size
-                                    'activation': 'relu',
-                                    'learning_rate': 0.0001,
-                                    'optimizer': 'adam',
-                                    'num_epochs': 10,
-                                    'loss_metric': 'softmax_cross_entropy',
-                                    'test_metric': 'accuracy'
-                                }
+        space['hyperparameters'] = {
+            'batch_size': 64,
+            'eval_batch_size': 64,
+            'activation': 'relu',
+            'learning_rate': 0.1,
+            'optimizer': 'adam',
+            'num_epochs': 20,
+            'loss_metric': 'mean_squared_error',
+            'test_metric': 'mean_squared_error',
+        }
         self.space = space
 
 
