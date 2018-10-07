45ae2b7510f2268341cb08ac554f4cfdf4b53c95
Deathn0t <romainegele@gmail.com>
2018-10-07 14:57:39 +0000

    update
    Former-commit-id: d963a90379a10c6cb2caebf960fb165a2478b8f7 [formerly 06dacfa1c8248257efbd720b45f30cd0092ff91d]
    Former-commit-id: 27b13387c16b4b3e6c9b4c1eb6d64f9dac79dcdc

--- deephyper/benchmarks/candleNT3Nas/problem.py
+++ deephyper/benchmarks/candleNT3Nas/problem.py
@@ -19,7 +19,7 @@ class Problem:
         space['create_structure'] = {
             'func': create_structure,
             'kwargs': {
-                'num_cells': 5:
+                'num_cells': 5
             }
         }
 
