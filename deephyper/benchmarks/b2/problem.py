caebe8aff7573fb36f60121c9db758e9c081039b
msalim <msalim@anl.gov>
2018-10-23 21:29:55 +0000

    Example of new HpProblem instantiation: benchmarks.b2.problem
    Former-commit-id: 00cb7fc1b4d7dd8ddeae54656211e3ed9e81ecb9 [formerly 6793ecb23ad184a192c2c6dd7124562785968044]
    Former-commit-id: 576c9ef847f61eac0fd7f20cf765e9a605e832c6

--- deephyper/benchmarks/b2/problem.py
+++ deephyper/benchmarks/b2/problem.py
@@ -1,33 +1,19 @@
+from deephyper.benchmarks import HpProblem
 from collections import OrderedDict
-class Problem():
-    def __init__(self):
-        space = OrderedDict()
-        space['epochs'] = (5, 30)
-        #bechmark specific parameters
-        space['rnn_type'] = ['LSTM', 'GRU', 'SimpleRNN']
-        space['nhidden'] = (1, 100)
-        #network parameters
-        space['activation'] = ['relu', 'elu', 'selu', 'tanh']
-        space['batch_size'] = (8, 1024)
-        space['dropout'] = (0.0, 1.0)
-        space['optimizer'] = ['sgd', 'rmsprop', 'adagrad', 'adadelta', 'adam', 'adamax', 'nadam']
-        # common optimizer parameters
-        #space['clipnorm'] = (1e-04, 1e01)
-        #space['clipvalue'] = (1e-04, 1e01)
-        # optimizer parameters
-        space['learning_rate'] = (1e-04, 1e01)
-        #space['momentum'] =  (0, 1e01)
-        #space['decay'] =  (0, 1e01)
-        #space['nesterov'] = [False, True]
-        #space['rho'] = (1e-04, 1e01)
-        #space['epsilon'] = (1e-08, 1e01)
-        #space['beta1'] = (1e-04, 1e01)
-        #space['beta2'] = (1e-04, 1e01)
 
-        self.space = space
-        self.params = list(self.space.keys())
-        self.starting_point = [5, 'LSTM', 1, 'relu', 8, 0.0, 'sgd', 1e-04] #1.0, 0.5, 0.01, 0, 0, False, 0.9, 1e-08, 0.9, 0.999]
+Problem = HpProblem()
+Problem.add_dim('epochs', (5,30), default=5)
+Problem.add_dim('rnn_type', ['LSTM', 'GRU', 'SimpleRNN'], default='LSTM')
+Problem.add_dim('nhidden', (1, 100), default=1)
+
+#network parameters
+Problem.add_dim('activation', ['relu', 'elu', 'selu', 'tanh'], default='relu')
+Problem.add_dim('batch_size', (8, 1024), default=8)
+Problem.add_dim('dropout', (0.0, 1.0), default=0.0)
+Problem.add_dim('optimizer', ['sgd', 'rmsprop', 'adagrad', 'adadelta', 'adam', 'adamax', 'nadam'], default='sgd')
+
+# common optimizer parameters
+Problem.add_dim('learning_rate', (1e-04, 1e01), default=1e-4)
 
 if __name__ == '__main__':
-    instance = Problem()
-    print(' '.join(f'--{k}={instance.starting_point[i]}' for i,k in enumerate(instance.params)))
+    print(Problem)
