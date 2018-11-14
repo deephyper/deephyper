92bc2b72c86a4aac3a448e7ac319d2879af09400
Deathn0t <romainegele@gmail.com>
2018-11-14 08:22:41 -0600

    update

--- deephyper/searches/nas/agent/nas_random.py
+++ deephyper/searches/nas/agent/nas_random.py
@@ -95,7 +95,6 @@ def train(num_episodes, seed, space, evaluator, num_episodes_per_batch):
 
         # MAKE ENV_NAS
         structure = space['create_structure']['func'](
-            tf.constant([[1., 1.]]),
             **space['create_structure']['kwargs']
         )
 
