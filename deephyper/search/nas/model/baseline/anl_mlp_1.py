ce91a9400e2f6d703963d53e6188ff65fe167dc0
Deathn0t <romainegele@gmail.com>
2018-11-16 14:22:25 -0600

    update

--- deephyper/search/nas/model/baseline/anl_mlp_1.py
+++ deephyper/search/nas/model/baseline/anl_mlp_1.py
@@ -1,32 +1,86 @@
-from deephyper.search.nas.cell.mlp import create_dense_cell_type1
-from deephyper.search.nas.cell.structure import create_sequential_structure
+import tensorflow as tf
 
+from deephyper.search.nas.model.space.op.basic import Connect
+from deephyper.search.nas.model.space.op.op1d import Dense, dropout_ops, Identity
+
+from deephyper.search.nas.model.space.node import Node
+from deephyper.search.nas.model.space.cell import Cell
+
+from deephyper.search.nas.cell.structure import create_seq_structure
+
+def create_dense_cell_type1(input_nodes):
+    """Dense type 1
+
+    Args:
+        input_nodes (list(Node)): possible inputs of the current cell.
+
+    Returns:
+        Cell: a Cell instance.
+    """
+    cell = Cell(input_nodes)
+
+    def create_block():
+        # first node of block
+        n1 = Node('N1')
+        for inpt in input_nodes:
+            n1.add_op(Connect(cell.graph, inpt, n1))
+
+        # second node of block
+        mlp_op_list = list()
+        mlp_op_list.append(Identity())
+        mlp_op_list.append(Dense(5, tf.nn.relu))
+        mlp_op_list.append(Dense(5, tf.nn.tanh))
+        mlp_op_list.append(Dense(10, tf.nn.relu))
+        mlp_op_list.append(Dense(10, tf.nn.tanh))
+        mlp_op_list.append(Dense(20, tf.nn.relu))
+        mlp_op_list.append(Dense(20, tf.nn.tanh))
+        n2 = Node('N2')
+        for op in mlp_op_list:
+            n2.add_op(op)
+
+        # third node of block
+        n3 = Node('N3')
+        for op in mlp_op_list:
+            n3.add_op(op)
+
+        # fourth node of block
+        n4 = Node('N4')
+        for op in mlp_op_list:
+            n4.add_op(op)
+
+        # fifth
+        n5 = Node('N5')
+        for op in dropout_ops:
+            n5.add_op(op)
+
+        # 5 Blocks
+        block = Block()
+        block.add_node(n1)
+        block.add_node(n2)
+        block.add_node(n3)
+        block.add_node(n4)
+        block.add_node(n5)
+
+        block.add_edge(n1, n2)
+        block.add_edge(n2, n3)
+        block.add_edge(n3, n4)
+        block.add_edge(n4, n5)
+        return block
+
+    block1 = create_block()
+    block2 = create_block()
+    block3 = create_block()
+    block4 = create_block()
+    block5 = create_block()
+
+    cell.add_block(block1)
+    cell.add_block(block2)
+    cell.add_block(block3)
+    cell.add_block(block4)
+    cell.add_block(block5)
+
+    cell.set_outputs()
+    return cell
 
 def create_structure(input_tensor, num_cells):
-    return create_sequential_structure(input_tensor, create_dense_cell_type1, num_cells)
-
-if __name__ == '__main__':
-    import tensorflow as tf
-    import numpy as np
-    from random import random
-
-    inpt = tf.constant(np.zeros((2, 28)), dtype=tf.float32)
-    num_cells = 3
-    net_struct = create_structure(inpt, num_cells)
-
-    cell_ops = [random() for _ in range(net_struct.num_nodes_cell(i=0)[0])]
-    ops = cell_ops * num_cells
-    print(f'ops: {ops}')
-    net_struct.set_ops(ops)
-    net_struct.draw_graphviz('anl_mlp_1.dot')
-
-    out = net_struct.create_tensor(train=False)
-
-    with tf.Session() as sess:
-        init = tf.global_variables_initializer()
-        sess.run(init)
-        res = sess.run(out)
-
-        print(f'res: {res}')
-        print(f'input shape: {inpt.get_shape()}')
-        print(f'outpt shape: {np.shape(res)}')
+    return create_seq_structure(input_tensor, create_dense_cell_type1, num_cells)
--- deephyper/search/nas/model/baseline/util/struct.py
+++ deephyper/search/nas/model/baseline/util/struct.py
@@ -24,3 +24,28 @@ def create_struct_full_skipco(input_shape, output_shape, create_cell, num_cells)
         network.add_cell_f(func, num=None)
 
     return network
+
+def create_seq_struct(input_shape, output_shape, create_cell, num_cells):
+    """
+        Create a KerasStructure object.
+
+        Args:
+            input_tensor (tensor): a tensorflow tensor object
+            create_cell (function): function that create a cell, take one argument (inputs: list(None))
+            num_cells (int): number of cells in the sequential structure
+
+        Return:
+            KerasStructure: the corresponding built structure.
+    """
+
+    network = KerasStructure(input_shape, output_shape)
+    input_node = network.input_node
+
+    func = lambda: create_cell([input_node])
+    network.add_cell_f(func)
+
+    func = lambda x: create_cell(x)
+    for i in range(num_cells-1):
+        network.add_cell_f(func, num=1)
+
+    return network
