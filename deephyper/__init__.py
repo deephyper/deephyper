103b8a53c3100021d0203317a754eb9cab808215
Bethany Lusch <blusch@alcfwl145.alcf.anl.gov>
2019-01-08 17:38:49 -0600

    docs more like those in develop branch: updated home page and created quick start page

---
+++ deephyper/__init__.py
@@ -0,0 +1,6 @@
+"""
+Deephyper is a Python package which provides a common interface for the implementation and study of scalable hyperparameter search methods. In this package we provide to the user different modules:
+    - ``benchmarks``: a set of problems for hyperparameter search which the user can use to compare our different search algorithms or as examples to build their own problems.
+    - ``evaluators``: a set of objects which help to run search on different systems and for different cases such as quick and light experiments or long and heavy runs.
+    - ``search``: a set of algorithms for hyperparameter search. You will also find a modular way to define new search algorithms.
+"""
--- docs/usage/workflow.rst
+++ docs/usage/workflow.rst
@@ -4,28 +4,7 @@ Workflow
 On local computer
 =================
 
-Command Line
-------------
-
-Assuming you have installed deephyper on your local environment.
-
-::
-
-    cd deephyper/deephyper/search
-    python ambs.py
-
-This is going to run an asynchronous model-based search (AMBS) with default parameters. To print the arguments of a search like AMBS just run :
-
-::
-
-    python ambs.py --help
-
-Now you can run AMBS with custom arguments :
-
-::
-
-    python ambs.py --problem deephyper.benchmarks.b2.problem.Problem --run deephyper.benchmarks.b2.babi_memnn.run
-
+TODO (see Quick Start for now)
 
 
 
