3a1c6ac9f5888de9ea757987ef20b58380839757
Bethany Lusch <blusch@alcfwl145.alcf.anl.gov>
2019-01-08 17:18:11 -0600

    add more documentation for search

--- deephyper/search/__init__.py
+++ deephyper/search/__init__.py
@@ -1,4 +1,9 @@
+"""
+The ``search`` module brings a modular way to implement new search algorithms. The ``Search`` class is abstract and has two subclasses ``deephyper.search.ambs`` and ``deephyper.search.ga``.
+"""
+
 from deephyper.search.search import Search
 from deephyper.search.ambs import AMBS
+from deephyper.search.ga import GA
 
 __all__ = ['Search', 'AMBS']
