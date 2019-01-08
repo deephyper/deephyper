92e36b5e0134ed43e5b11c718decb1d607dd6709
Bethany Lusch <blusch@alcfwl145.alcf.anl.gov>
2019-01-08 16:35:58 -0600

    add to docs for evaluators

--- deephyper/evaluators/__init__.py
+++ deephyper/evaluators/__init__.py
@@ -1,2 +1,6 @@
+"""
+The goal of the evaluators module is to have a set of objects which can help us to run our task on different environments and with different system settings/properties.
+"""
+
 from deephyper.evaluators.evaluate import Evaluator, Encoder
 __all__ = ['Evaluator', 'Encoder']
