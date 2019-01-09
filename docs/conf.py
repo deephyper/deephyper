4d0908a8404b22fdcab1c22d112231ebf3b2216f
Romain Egele <romainegele@gmail.com>
2019-01-09 09:56:11 +0100

    Update conf.py
    edit version in docs' conf

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = u'Argonne'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.1'
+release = u'0.0.3'
 
 
 # -- General configuration ---------------------------------------------------
