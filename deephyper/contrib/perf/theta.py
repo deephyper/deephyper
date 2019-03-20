cbcb679ff76f9b9b7a72a98b19a0f39eecf931e5
Deathn0t <romainegele@gmail.com>
2019-03-20 21:22:27 +0100

    update perf settings

--- deephyper/contrib/perf/theta.py
+++ deephyper/contrib/perf/theta.py
@@ -2,19 +2,25 @@ import os
 import tensorflow as tf
 from tensorflow.keras import backend as K
 
+
+"""Performance settings for Theta
+
+OMP_NUM_THREADS='62' should be set before importing tensorflow
+"""
+
 def get_session_conf():
     """Set env variables for better performance on Theta.
 
-    Return:
-        A tf.ConfigProto object with specific settings.
+        Return:
+                A tf.ConfigProto object with specific settings.
     """
     os.environ['KMP_BLOCKTIME'] = '0'
     os.environ['KMP_AFFINITY'] = 'granularity=fine,compact,1,0'
-    os.environ['OMP_NUM_THREADS'] = '62'
 
-    session_conf = tf.ConfigProto(
-            intra_op_parallelism_threads=int(os.environ['OMP_NUM_THREADS'])
-            )
+    if not(os.environ.get('OMP_NUM_THREADS') is None):
+        session_conf = tf.ConfigProto(
+                intra_op_parallelism_threads=int(os.environ['OMP_NUM_THREADS'])
+                )
 
     return session_conf
 
