f0edef1379e595dccf253cb3d285f609437e5148
Deathn0t <romainegele@gmail.com>
2019-05-03 09:12:21 -0500

    updated ppo

--- deephyper/search/nas/ppo.py
+++ deephyper/search/nas/ppo.py
@@ -1,7 +1,6 @@
 import os
 
 from deephyper.search.nas.nas_search import NeuralArchitectureSearch
-from deephyper.evaluator._balsam import BalsamEvaluator  # TODO: async kw
 
 try:
     from mpi4py import MPI
@@ -12,15 +11,10 @@ except ImportError:
 class Ppo(NeuralArchitectureSearch):
     def __init__(self, problem, run, evaluator, network, **kwargs):
         if MPI is None:
-            print('#DEBUG# MPI is None')
             nenvs = 1
         else:
             nranks = MPI.COMM_WORLD.Get_size()
-            print('#DEBUG# MPI is not None')
-            print('#DEBUG# nranks=', nranks)
-            print('#DEBUG# isinstance(evaluator, BalsamEvaluator)==',
-                  isinstance(evaluator, BalsamEvaluator))
-            if isinstance(evaluator, BalsamEvaluator):  # TODO: async kw
+            if evaluator == 'balsam':  # TODO: async is a kw
                 balsam_launcher_nodes = int(
                     os.environ.get('BALSAM_LAUNCHER_NODES', 1))
                 deephyper_workers_per_node = int(
@@ -29,14 +23,6 @@ class Ppo(NeuralArchitectureSearch):
                 n_free_nodes = balsam_launcher_nodes - nranks  # Number of free nodes
                 free_workers = n_free_nodes * deephyper_workers_per_node  # Number of free workers
                 nenvs = free_workers // nagents
-                dd = dict(balsam_launcher_nodes=balsam_launcher_nodes,
-                          deephyper_workers_per_node=deephyper_workers_per_node,
-                          nagents=nagents,
-                          nranks=nranks,
-                          n_free_nodes=n_free_nodes,
-                          free_workers=free_workers,
-                          nenvs=nenvs)
-                print('#DEBUG# dd=', dd)
             else:
                 nenvs = 1
 
