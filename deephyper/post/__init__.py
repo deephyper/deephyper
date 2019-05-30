af42bfff5afed8f1056fe46890d9c49e37651d39
Deathn0t <romainegele@gmail.com>
2019-05-30 16:59:03 -0500

    post training pipeline

---
+++ deephyper/post/__init__.py
@@ -0,0 +1,2 @@
+"""The ``post`` sub-package is dedicated to post-training classes. In deephyper post-training is the act of traing a DNN model after a search completed in order to mesure its maximal accuracy.
+"""
