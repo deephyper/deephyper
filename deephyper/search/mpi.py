7acffc5c0e69776dea685d682ea7c2c01290c807
Matthieu Dorier <mdorier@anl.gov>
2019-06-05 04:19:12 -0500

    cleaned up mpi by putting code into classes

--- deephyper/search/mpi.py
+++ deephyper/search/mpi.py
@@ -1,9 +1,12 @@
 import signal
 import logging
+import argparse
 from mpi4py import MPI
 from deephyper.search import util
 from deephyper.evaluator.evaluate import Evaluator
-from .hps.ambs import AMBS, on_exit
+from .search import Search
+from .hps.ambs import AMBS, on_exit as ambs_on_exit
+from .hps.ga import GA, on_exit as ga_on_exit
 
 logger = logging.getLogger(__name__)
 
@@ -14,7 +17,6 @@ class MPIWorker():
         self.rank = comm.Get_rank()
         self.problem = util.generic_loader(problem, 'Problem')
         self.run_func = util.generic_loader(run, 'run')
-        logger.info('Evaluator in MPI worker will execute the function: '+run)
         self.evaluator = Evaluator.create(self.run_func, method=evaluator)
 
     def _exec_eval(self, x):
@@ -30,17 +32,52 @@ class MPIWorker():
             y = self._exec_eval(exec_info['args'])
             comm.send(y, dest=0, tag=0)
 
+class MPIManager():
+
+    def __init__(self, comm, **kwargs):
+        self.comm = comm
+        self.args = kwargs
+        self.args['evaluator'] = '__mpiPool'
+        search_cls = self._get_search_cls(**self.args)
+        self.search = search_cls(**self.args)
+
+    @staticmethod
+    def parse_args():
+        parser = argparse.ArgumentParser(conflict_handler='resolve')
+        parser.add_argument('--search',
+            choices=["AMBS", "GA"],
+            help='type of HPS search method to use'
+            )
+        args, remaining_cmd =  parser.parse_known_args()
+        search_cls = MPIManager._get_search_cls(**vars(args))
+        remaining_args = search_cls.parse_args(remaining_cmd)
+        setattr(remaining_args, 'search', args.search)
+        return remaining_args
+
+    def run(self):
+        if(isinstance(self.search, AMBS)):
+            signal.signal(signal.SIGINT, ambs_on_exit)
+            signal.signal(signal.SIGTERM, ambs_on_exit)
+            self.search.main()
+        elif(isinstance(self.search, GA)):
+            self.search.run()
+
+    @staticmethod
+    def _get_search_cls(**kwargs):
+        search_cls = None
+        if(kwargs['search'] == 'AMBS'):
+            search_cls = AMBS
+        elif(kwargs['search'] == 'GA'):
+            search_cls = GA
+        return search_cls
 
 if __name__ == "__main__":
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
-    args = AMBS.parse_args()
+    args = MPIManager.parse_args()
     if(rank == 0):
-        args.evaluator = '__mpiPool'
-        search = AMBS(**vars(args))
-        signal.signal(signal.SIGINT, on_exit)
-        signal.signal(signal.SIGTERM, on_exit)
-        search.main()
+        master = MPIManager(comm, **vars(args))
+        master.run()
     else:
-        worker = MPIWorker(MPI.COMM_WORLD, **vars(args))
+        worker = MPIWorker(comm, **vars(args))
         worker.run()
