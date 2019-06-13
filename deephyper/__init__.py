b57d8ca4bfa0d7417b99e4580673cd0affcee137
Prasanna <prasannaprakash@gmail.com>
2019-06-13 12:01:07 -0500

    docs update

--- deephyper/__init__.py
+++ deephyper/__init__.py
@@ -1,8 +1,17 @@
 """
-Deephyper is a Python package which provides a common interface for the implementation and study of scalable hyperparameter and neural architecture search methods. In this package we provide to the user different modules:
-    - ``benchmark``: a set of problems for hyperparameter or neural architecture search which the user can use to compare our different search algorithms or as examples to build their own problems.
-    - ``evaluator``: a set of objects which help to run search on different systems and for different cases such as quick and light experiments or long and heavy runs.
-    - ``search``: a set of algorithms for hyperparameter and neural architecture search. You will also find a modular way to define new search algorithms and specific sub modules for hyperparameter or neural architecture search.
+DeepHyper is a scalable automated machine learning (`AutoML <https://en.wikipedia.org/wiki/Automated_machine_learning>`_) package for developing deep neural networks for scientific applications. 
+It comprises two components: 
+    - ``Neural architecture search (NAS)``: It is designed for automatically searching for high-performing the deep neural network architecture. 
+    - ``Hyperparameter search (HPS)``: It is designed for automatically searching for high-performing hyperparameters for a given deep neural network architecture. 
+
+DeepHyper provides an infrastructure that targets experimental research in NAS and HPS methods, scalability, and portability across diverse supercomputers. 
+It comprises three modules:
+    - ``benchmark``: Set of test problems for NAS and HPS that can be used for comparing different search methods. They can serve as examples to build new user-defined problems.
+    - ``evaluator``: Set of objects to run NAS and HPS on different target systems (from laptop to supercomputers) covering different use cases (quick/light experiments on laptop for testing and development to large production runs on supercomputers).
+    - ``search``: Set of search methods for NAS and HPS. It provides a modular way to define new search HPS and NAS search methods and submodules for implementing HPS and NAS.
+
+DeepHyper installation requires **Python 3.6**.
+
 """
 from deephyper.__version__ import __version__
 name = 'deephyper'
