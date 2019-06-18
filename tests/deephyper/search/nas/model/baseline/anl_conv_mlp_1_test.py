5dfae78270b0357070ce77ecc094a27b955b0ccd
Deathn0t <romainegele@gmail.com>
2019-06-18 08:54:27 -0500

    corrected few tests

--- tests/deephyper/search/nas/model/baseline/anl_conv_mlp_1_test.py
+++ tests/deephyper/search/nas/model/baseline/anl_conv_mlp_1_test.py
@@ -2,7 +2,6 @@ def test_create_structure():
     from random import random, seed
     from deephyper.search.nas.model.space.structure import KerasStructure
     from deephyper.search.nas.model.baseline.anl_conv_mlp_1 import create_structure
-    from deephyper.core.model_utils import number_parameters
     from tensorflow.keras.utils import plot_model
     import tensorflow as tf
     seed(10)
@@ -12,7 +11,8 @@ def test_create_structure():
 
     # ops = [random() for i in range(structure.num_nodes)]
     # ops = [0 for i in range(structure.num_nodes)]
-    ops = [0.2, 0.8, 0.0, 0.8, 0.0, 0.0, 0.6, 0.8, 0.0, 0.2, 0.6, 0.2, 0.0, 0.4, 0.8, 0.6, 0.2, 0.8, 0.4, 0.0, 0.4, 0.8, 0.2, 0.8, 0.4, 0.0, 0.6, 0.4, 0.4, 0.0, 0.6, 0.4, 0.0, 0.8, 0.0, 0.6, 0.6, 0.4, 0.4, 0.2, 0.0, 0.6, 0.2, 0.2, 0.4]
+    ops = [0.2, 0.8, 0.0, 0.8, 0.0, 0.0, 0.6, 0.8, 0.0, 0.2, 0.6, 0.2, 0.0, 0.4, 0.8, 0.6, 0.2, 0.8, 0.4, 0.0, 0.4, 0.8,
+           0.2, 0.8, 0.4, 0.0, 0.6, 0.4, 0.4, 0.0, 0.6, 0.4, 0.0, 0.8, 0.0, 0.6, 0.6, 0.4, 0.4, 0.2, 0.0, 0.6, 0.2, 0.2, 0.4]
     print('num ops: ', len(ops))
     structure.set_ops(ops)
     structure.draw_graphviz('graph_anl_conv_mlp_1_test.dot')
@@ -33,10 +33,8 @@ def test_create_structure():
         print(f'shape(x): {np.shape(x)}')
     print(f'shape(y): {np.shape(y)}')
 
-    total_parameters = number_parameters()
-    print('total_parameters: ', total_parameters)
-
     # assert np.shape(y) == (1, 1), f'Wrong output shape {np.shape(y)} should be {(1, 1)}'
+
 
 if __name__ == '__main__':
     test_create_structure()
--- tests/deephyper/search/nas/model/baseline/anl_mlp_2_test.py
+++ tests/deephyper/search/nas/model/baseline/anl_mlp_2_test.py
@@ -2,10 +2,9 @@ def test_create_structure():
     from random import random, seed
     from deephyper.search.nas.model.space.structure import KerasStructure
     from deephyper.search.nas.model.baseline.anl_mlp_2 import create_structure
-    from deephyper.core.model_utils import number_parameters
     seed(10)
 
-    structure = create_structure([(10,1), (10,1)], (1,), 5)
+    structure = create_structure([(10, 1), (10, 1)], (1,), 5)
     assert type(structure) is KerasStructure
 
     ops = [random() for i in range(structure.num_nodes)]
@@ -21,10 +20,9 @@ def test_create_structure():
     print(f'shape(x): {np.shape(x)}')
     print(f'shape(y): {np.shape(y)}')
 
-    nparameters = number_parameters()
-    print('number of parameters: ', nparameters)
+    assert np.shape(y) == (
+        1, 1), f'Wrong output shape {np.shape(y)} should be {(1, 1)}'
 
-    assert np.shape(y) == (1, 1), f'Wrong output shape {np.shape(y)} should be {(1, 1)}'
 
 if __name__ == '__main__':
     test_create_structure()
