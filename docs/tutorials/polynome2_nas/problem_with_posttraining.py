f936df905b1a9779f05b4cdcec481ed6cc1138ad
Deathn0t <romainegele@gmail.com>
2019-06-27 11:55:58 -0500

    update NAS tutorial

--- docs/tutorials/polynome2_nas/problem_with_posttraining.py
+++ docs/tutorials/polynome2_nas/problem_with_posttraining.py
@@ -1,14 +1,10 @@
-from deephyper.benchmark import NaProblem
-
 from nas_problems.polynome2.load_data import load_data
-from nas_problems.polynome2.preprocessing import minmaxstdscaler
 from nas_problems.polynome2.structure import create_structure
+from deephyper.benchmark import NaProblem
 
 Problem = NaProblem()
 
 Problem.load_data(load_data, size=1e4)
-
-Problem.preprocessing(minmaxstdscaler)
 
 Problem.search_space(create_structure)
 
@@ -24,6 +20,23 @@ Problem.loss('mse')
 Problem.metrics(['r2'])
 
 Problem.objective('val_r2__last')
+
+Problem.post_training(
+    num_epochs=60,
+    metrics=['mse', 'r2'],
+    model_checkpoint={
+        'monitor': 'val_r2',
+        'mode': 'max',
+        'save_best_only': True,
+        'verbose': 1
+    },
+    early_stopping={
+        'monitor': 'val_r2',
+        'mode': 'max',
+        'verbose': 1,
+        'patience': 5
+    }
+)
 
 if __name__ == '__main__':
     print(Problem)
