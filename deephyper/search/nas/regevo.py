4539f73ed0568f5663c6e17961c5f726a95d2419
Deathn0t <romainegele@gmail.com>
2019-07-17 16:44:08 -0500

    update

--- deephyper/search/nas/regevo.py
+++ deephyper/search/nas/regevo.py
@@ -12,7 +12,7 @@ except ImportError:
 
 
 class RegularizedEvolution(Search):
-    """The effort devoted to hand-crafting neural network image classifiers has motivated the use of architecture search to discover them automatically. Although evolutionary algorithms have been repeatedly applied to neural network topologies, the image classifiers thus discovered have remained inferior to human-crafted ones. Here, we evolve an image classifier---AmoebaNet-A---that surpasses hand-designs for the first time. To do this, we modify the tournament selection evolutionary algorithm by introducing an age property to favor the younger genotypes. Matching size, AmoebaNet-A has comparable accuracy to current state-of-the-art ImageNet models discovered with more complex architecture-search methods. Scaled to larger size, AmoebaNet-A sets a new state-of-the-art 83.9% / 96.6% top-5 ImageNet accuracy. In a controlled comparison against a well known reinforcement learning algorithm, we give evidence that evolution can obtain results faster with the same hardware, especially at the earlier stages of the search. This is relevant when fewer compute resources are available. Evolution is, thus, a simple method to effectively discover high-quality architectures.
+    """Regularized evolution.
 
     https://arxiv.org/abs/1802.01548
 
