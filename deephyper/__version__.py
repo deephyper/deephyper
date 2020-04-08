0675bd458accd0200771d8860badf9190fbf0584
Deathn0t <romainegele@gmail.com>
2020-04-08 10:57:59 +0200

    edited verison to 0.1.4

--- deephyper/__version__.py
+++ deephyper/__version__.py
@@ -1,6 +1,6 @@
-VERSION = (0, 1, 3)
+VERSION = (0, 1, 4)
 
-__version__ = '.'.join(map(str, VERSION))
+__version__ = ".".join(map(str, VERSION))
 
 # alpha/beta/rc tags
-__version_suffix__ = ''
+__version_suffix__ = ""
