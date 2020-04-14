abdb2d0ba75002336eb16c7180ffbe0b69c3c14c
Deathn0t <romainegele@gmail.com>
2020-04-14 18:02:54 +0200

    updated BaseClassifierPipeline

--- deephyper/baseline/classifier.py
+++ deephyper/baseline/classifier.py
@@ -15,7 +15,7 @@ class BaseClassifierPipeline:
     >>> load_data = lambda : load_digits(return_X_y=True)
     >>> baseline_classifier = BaseClassifierPipeline(KNeighborsClassifier(), load_data)
     >>> baseline_classifier.run() # doctest:+ELLIPSIS
-    Not Weighted Accuracy:...
+    Not Weighted Accuracy (Test set):...
     """
 
     def __init__(
@@ -27,29 +27,52 @@ class BaseClassifierPipeline:
         self.clf = clf
         self.seed = seed
         self.load_data_func = load_data_func
+        self.preproc = minmaxstdscaler()
 
-    def run(self):
-
-        # loading the data
+    def load_data(self):
         try:
-            (X_train, X_test), (y_train, y_test) = self.load_data_func()
+            (X_train, y_train), (X_test, y_test) = self.load_data_func()
         except:
             X, y = self.load_data_func()
             X_train, X_test, y_train, y_test = train_test_split(
                 X, y, test_size=0.33, random_state=self.seed
             )
 
+        return X_train, X_test, y_train, y_test
+
+    def run(self):
+
+        # loading the data
+        X_train, X_test, y_train, y_test = self.load_data()
+
         # preprocessing the data
-        preproc = minmaxstdscaler()
-        X_train = preproc.fit_transform(X_train)
-        X_test = preproc.transform(X_test)
+        X_train = self.preproc.fit_transform(X_train)
+        X_test = self.preproc.transform(X_test)
 
         self.clf.fit(X_train, y_train)
 
         y_pred = self.clf.predict(X_test)
         acc_not_weighted = accuracy_score(y_test, y_pred)
 
-        print("Not Weighted Accuracy: ", acc_not_weighted)
+        print("Not Weighted Accuracy (Test set): ", acc_not_weighted)
+
+    def evaluate(self, metric):
+
+        X_train, X_test, y_train, y_test = self.load_data()
+
+        X_train = self.preproc.transform(X_train)
+        X_test = self.preproc.transform(X_test)
+
+        y_pred = self.clf.predict(X_train)
+        score_train = metric(y_train, y_pred)
+
+        y_pred = self.clf.predict(X_test)
+        score_test = metric(y_test, y_pred)
+
+        metric_name = metric.__name__
+
+        print(f"{metric_name} on Train: ", score_train)
+        print(f"{metric_name} on Test: ", score_test)
 
 
 if __name__ == "__main__":
