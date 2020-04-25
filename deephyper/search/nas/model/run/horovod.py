3a92c65d8e642de5dc6bc4bd72361c7508741aa3
Deathn0t <romainegele@gmail.com>
2020-04-25 12:20:00 +0200

    WIP template of horovod function for nas

---
+++ deephyper/search/nas/model/run/horovod.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+import horovod.tensorflow.keras as hvd
+
+# Initialize Horovod
+hvd.init()
+
+# Pin GPU to be used to process local rank (one GPU per process)
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+if gpus:
+    tf.config.experimental.set_visible_devices(gpus[hvd.local_rank()], 'GPU')
+
+# Build model and dataset
+dataset = ...
+model = ...
+opt = tf.optimizers.Adam(0.001 * hvd.size())
+
+# Horovod: add Horovod DistributedOptimizer.
+opt = hvd.DistributedOptimizer(opt)
+
+# Horovod: Specify `experimental_run_tf_function=False` to ensure TensorFlow
+# uses hvd.DistributedOptimizer() to compute gradients.
+mnist_model.compile(loss=tf.losses.SparseCategoricalCrossentropy(),
+                    optimizer=opt,
+                    metrics=['accuracy'],
+                    experimental_run_tf_function=False)
+
+callbacks = [
+    # Horovod: broadcast initial variable states from rank 0 to all other processes.
+    # This is necessary to ensure consistent initialization of all workers when
+    # training is started with random weights or restored from a checkpoint.
+    hvd.callbacks.BroadcastGlobalVariablesCallback(0),
+]
+
+# Horovod: save checkpoints only on worker 0 to prevent other workers from corrupting them.
+if hvd.rank() == 0:
+    callbacks.append(tf.keras.callbacks.ModelCheckpoint('./checkpoint-{epoch}.h5'))
+
+model.fit(dataset,
+          steps_per_epoch=500 // hvd.size(),
+          callbacks=callbacks,
+          epochs=24,
+          verbose=1 if hvd.rank() == 0 else 0)
