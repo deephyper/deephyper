d89cd576c0148b47d9419383d594f4913c3f3cfe
Deathn0t <romainegele@gmail.com>
2020-04-29 15:57:34 +0200

    updated doc

--- deephyper/benchmark/nas/nasbench101/__init__.py
+++ deephyper/benchmark/nas/nasbench101/__init__.py
@@ -1,14 +1,15 @@
 """
-NasBench 101, a benchmark using: https://github.com/google-research/nasbench
-To download the full data set use::
+NasBench-101 is a database of trained neural networks corresponding to a specific cell-based search space for convolution neural networks (`Link to the paper <https://arxiv.org/abs/1902.09635>`_). To use this benchmark you need to install the ``nasbench`` package available at `github.com/google-research/nasbench <https://github.com/google-research/nasbench>_`.
+
+Then, to download the full data set use::
 
     sh download_full.sh
 
-To Download the dataset only with models evaluated for 108 epochs::
+Or, if you want to Download the dataset only with models evaluated for 108 epochs::
 
     sh download_only109.sh
 
-Example usage with the regularized evolution search::
+An example usage with the regularized evolution search is::
 
     python -m deephyper.search.nas.regevo --problem deephyper.benchmark.nas.nasbench101.problem.Problem --evaluator threadPool --run deephyper.benchmark.nas.nasbench101.run.run --max-evals 10000
 """
--- docs/search/nas/benchmarks.rst
+++ docs/search/nas/benchmarks.rst
@@ -23,6 +23,4 @@ NAS Benchmarks
 NasBench-101
 ------------
 
-NasBench-101 is a database of trained neural networks corresponding to a specific cell-based search space for convolution neural networks (`Link to the paper <https://arxiv.org/abs/1902.09635>`_).
-
 .. automodule:: deephyper.benchmark.nas.nasbench101
