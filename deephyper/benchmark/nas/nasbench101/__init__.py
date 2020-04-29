c77af1305104b91d2e9019fee1bc3a93b38d0622
Deathn0t <romainegele@gmail.com>
2020-04-29 16:10:22 +0200

    corrected error in doc

--- deephyper/benchmark/nas/nasbench101/__init__.py
+++ deephyper/benchmark/nas/nasbench101/__init__.py
@@ -1,5 +1,5 @@
 """
-NasBench-101 is a database of trained neural networks corresponding to a specific cell-based search space for convolution neural networks (`Link to the paper <https://arxiv.org/abs/1902.09635>`_). To use this benchmark you need to install the ``nasbench`` package available at `github.com/google-research/nasbench <https://github.com/google-research/nasbench>_`.
+NasBench-101 is a database of trained neural networks corresponding to a specific cell-based search space for convolution neural networks (`Link to the paper <https://arxiv.org/abs/1902.09635>`_). To use this benchmark you need to install the ``nasbench`` package available at `github.com/google-research/nasbench <https://github.com/google-research/nasbench>`_.
 
 Then, to download the full data set use::
 
@@ -7,7 +7,7 @@ Then, to download the full data set use::
 
 Or, if you want to Download the dataset only with models evaluated for 108 epochs::
 
-    sh download_only109.sh
+    sh download_only108.sh
 
 An example usage with the regularized evolution search is::
 
