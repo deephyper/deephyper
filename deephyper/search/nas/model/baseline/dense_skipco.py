14c56b2e66e45f25773f749811ba8120460c478e
Deathn0t <romainegele@gmail.com>
2020-05-14 20:07:34 +0200

    added swish function in model.baseline.dense_skipco

--- deephyper/search/nas/model/baseline/dense_skipco.py
+++ deephyper/search/nas/model/baseline/dense_skipco.py
@@ -10,21 +10,24 @@ from ..space.op.merge import AddByProjecting
 from ..space.op.op1d import Dense, Identity
 
 
-def add_dense_to_(node):
-    node.add_op(Identity()) # we do not want to create a layer in this case
+def swish(x):
+    return x * tf.nn.sigmoid(x)
 
-    activations = [None, tf.nn.relu, tf.nn.tanh, tf.nn.sigmoid]
+
+def add_dense_to_(node):
+    node.add_op(Identity())  # we do not want to create a layer in this case
+
+    activations = [None, swish, tf.nn.relu, tf.nn.tanh, tf.nn.sigmoid]
     for units in range(16, 97, 16):
         for activation in activations:
             node.add_op(Dense(units=units, activation=activation))
 
 
-def create_search_space(input_shape=(10,),
-                        output_shape=(7,),
-                        num_layers=10,
-                        *args, **kwargs):
+def create_search_space(
+    input_shape=(10,), output_shape=(7,), num_layers=10, regression=True, *args, **kwargs
+):
 
-    arch = AutoKSearchSpace(input_shape, output_shape, regression=True)
+    arch = AutoKSearchSpace(input_shape, output_shape, regression=regression)
     source = prev_input = arch.input_nodes[0]
 
     # look over skip connections within a range of the 3 previous nodes
@@ -40,7 +43,7 @@ def create_search_space(input_shape=(10,),
         cell_output = vnode
 
         cmerge = ConstantNode()
-        cmerge.set_op(AddByProjecting(arch, [cell_output], activation='relu'))
+        cmerge.set_op(AddByProjecting(arch, [cell_output], activation="relu"))
 
         for anchor in anchor_points:
             skipco = VariableNode()
@@ -51,6 +54,5 @@ def create_search_space(input_shape=(10,),
         # ! for next iter
         prev_input = cmerge
         anchor_points.append(prev_input)
-
 
     return arch
