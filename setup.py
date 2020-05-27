c4ad89416df1118ddb6e9f0a1ca6cb79d0205c03
Deathn0t <romainegele@gmail.com>
2020-05-27 08:43:50 +0200

    update setup.py to avoid conflict between tensorflow and tensorflow-gpu

--- setup.py
+++ setup.py
@@ -55,11 +55,12 @@ if on_theta:  # --system-site-packages
     # we want to use the default mpi4py from cray environment
     REQUIRED.append("mpi4py")
     REQUIRED.remove("tensorflow>=1.13.1,<=1.15.2")
-    REQUIRED.append("tensorflow==1.13.1")
+    REQUIRED.append("tensorflow-gpu>=1.13.1,<=1.15.2")
 elif not on_rtd and not on_gpu:
     REQUIRED.append("mpi4py>=3.0.0")
 elif on_gpu:
-    REQUIRED.append("tensorflow-gpu>=1.13.1")
+    REQUIRED.remove("tensorflow>=1.13.1,<=1.15.2")
+    REQUIRED.append("tensorflow-gpu>=1.13.1,<=1.15.2")
     REQUIRED.append("mpi4py")
 else:
     REQUIRED.append("Sphinx>=1.8.2")
