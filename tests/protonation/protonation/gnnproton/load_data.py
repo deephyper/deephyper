d13beba6301741411db52d5e7a3be040525bd585
sjiang87 <sjiang87@wisc.edu>
2020-07-06 23:46:12 -0500

    gnn with MPNN new

--- tests/protonation/protonation/gnnproton/load_data.py
+++ tests/protonation/protonation/gnnproton/load_data.py
@@ -1,58 +1,36 @@
 import os
-import numpy as np
 import pickle
-# from protonation.gnnproton.Data_Processing import molecule
-# from protonation.gnnproton.train_utils import load_data_split, load_dict, load_data_func
 
 
-def load_data():
-    # define necessary parameters
-    elem_list = ['C', 'O', 'H', 'unknown']  # species of atoms
-    atom_fdim = len(elem_list) + 6 + 6 + 6 + 1  # number of atomic features
-    bond_fdim = 6  # number of bond features
-    max_nb = 6  # maximum number of neighbors
-    output_fdim = 1  # output feature dimension
-    max_atom = 31  # maximum atoms in the molecule
-
+def load_data(edge=False):
     # define directories
     if os.path.exists(r"/mnt/d/machinelearning2"):
         DATA_DIR = r"/mnt/d/machinelearning2/ANL/deephyper/tests/protonation/protonation/gnnproton/"
     elif os.path.exists(r"/blues/gpfs/home/shengli.jiang"):
         DATA_DIR = r"/blues/gpfs/home/shengli.jiang/deephyper/tests/protonation/protonation/gnnproton/"
     else:
-    	DATA_DIR = r"D:\\machinelearning2\\ANL\\deephyper\\tests\\protonation\\protonation\\gnnproton\\"
+        DATA_DIR = r"D:\\machinelearning2\\ANL\\deephyper\\tests\\protonation\\protonation\\gnnproton\\"
 
-    # data_dict = load_dict(name=DATA_DIR)
-    # train, valid = load_data_split(TRAIN_DIR, VALID_DIR)
-    # train = np.array(train)
-    # valid = np.array(valid)
-    # test_data_dict = load_dict(name=TEST_DATA_DIR)
-    # test, _ = load_data_split(TEST_DIR, VALID_DIR)
-    #
-    # A_train, X_train, E_train, m_train, y_train = load_data_func(train, data_dict)
-    # A_valid, X_valid, E_valid, m_valid, y_valid = load_data_func(valid, data_dict)
-    #
-    # with open("./protonation_data.pickle", "wb") as handle:
-    #     pickle.dump([A_train, X_train, E_train, m_train, y_train], handle)
-    #     pickle.dump([A_valid, X_valid, E_valid, m_valid, y_valid], handle)
-
-
-    with open(DATA_DIR+"protonation_data.pickle", "rb") as handle:
+    with open(DATA_DIR + "protonation_data.pickle", "rb") as handle:
         [A_train, X_train, E_train, m_train, y_train] = pickle.load(handle)
         [A_valid, X_valid, E_valid, m_valid, y_valid] = pickle.load(handle)
-    print(f"================ Load Data ================")
-    print(f"A_train shape: {np.shape(A_train)}")
-    print(f"X_train shape: {np.shape(X_train)}")
-    print(f"E_train shape: {np.shape(E_train)}")
-    print(f"m_train shape: {np.shape(m_train)}")
-    print(f"y_train shape: {np.shape(y_train)}")
-    print(f"A_valid shape: {np.shape(A_valid)}")
-    print(f"X_valid shape: {np.shape(X_valid)}")
-    print(f"E_valid shape: {np.shape(E_valid)}")
-    print(f"m_valid shape: {np.shape(m_valid)}")
-    print(f"y_valid shape: {np.shape(y_valid)}")
-    print(f"================ Load Data Finished ================")
-    return ([X_train, A_train, E_train, m_train], y_train), ([X_valid, A_valid, E_valid, m_valid], y_valid)
+    A_train = A_train.reshape(A_train.shape[0], A_train.shape[1] * A_train.shape[2], 1)
+    A_valid = A_valid.reshape(A_valid.shape[0], A_valid.shape[1] * A_valid.shape[2], 1)
+    E_train = E_train.reshape(E_train.shape[0], E_train.shape[1] * E_train.shape[2], E_train.shape[3])
+    E_valid = E_valid.reshape(E_valid.shape[0], E_valid.shape[1] * E_valid.shape[2], E_valid.shape[3])
+
+    print(f"The task is protonation.")
+    print(f"Max atom is {X_train.shape[1]}, N feature is {X_train.shape[2]}, E feature is {E_train.shape[-1]}.")
+    print(f"Train size: {X_train.shape[0]}, valid size: {X_valid.shape[0]}, test size: None")
+
+    if edge is False:
+        return ([X_train, A_train, E_train], y_train), \
+               ([X_valid, A_valid, E_valid], y_valid)
+    else:
+        return ([X_train, A_train, E_train], y_train), \
+               ([X_valid, A_valid, E_valid], y_valid), \
+               ([X_valid, A_valid, E_valid], y_valid), \
+               'protonation', None
 
 
 if __name__ == '__main__':
--- tests/protonation/protonation/gnnproton/search_space.py
+++ tests/protonation/protonation/gnnproton/search_space.py
@@ -1,16 +1,15 @@
 import collections
 import tensorflow as tf
-from deephyper.search.nas.model.space import AutoKSearchSpace, KSearchSpace
+from deephyper.search.nas.model.space import KSearchSpace
 from deephyper.search.nas.model.space.node import ConstantNode, VariableNode
-from deephyper.search.nas.model.space.op.merge import AddByProjecting
-from deephyper.search.nas.model.space.op.gnn import EdgeConditionedConv2, GlobalMaxPool2, GraphIdentity, GlobalAvgPool2,\
-    GlobalSumPool2, Apply1DMask
+from deephyper.search.nas.model.space.op.merge import AddByProjecting, Concatenate
+from deephyper.search.nas.model.space.op.gnn import GlobalAvgPool, GlobalSumPool, GlobalMaxPool, MPNN
 from deephyper.search.nas.model.space.op.op1d import Dense, Identity, Flatten
 from deephyper.search.nas.model.space.op.connect import Connect
 from deephyper.search.nas.model.space.op.basic import Tensor
 
 
-def add_gcn_to_(node):
+def add_gat_to_(node):
     """
     Function to add operations to graph convolution variable node
     Args:
@@ -19,31 +18,34 @@ def add_gcn_to_(node):
     Returns:
 
     """
-    node.add_op(GraphIdentity())
-    activations = [tf.nn.relu, tf.nn.tanh]
+    # node.add_op(GraphIdentity())
+    state_dims = [8, 16, 32]
+    Ts = [1, 2, 3]
+    attn_methods = ['const', 'gcn', 'gat', 'sym-gat']
+    attn_heads = [1, 2, 4, 6]
+    aggr_methods = ['max', 'mean', 'sum']
+    update_methods = ['gru', 'mlp']
+    activations = [tf.nn.sigmoid, tf.nn.tanh, tf.nn.relu, tf.keras.activations.linear, tf.nn.softplus, tf.nn.leaky_relu,
+                   tf.nn.relu6, tf.nn.elu]
 
-    for channels in [16, 32]:
-        for activation in activations:
-            node.add_op(EdgeConditionedConv2(channels=channels, activation=activation))
+    for state_dim in state_dims:
+        for T in Ts:
+            for attn_method in attn_methods:
+                for attn_head in attn_heads:
+                    for aggr_method in aggr_methods:
+                        for update_method in update_methods:
+                            for activation in activations:
+                                node.add_op(MPNN(state_dim=state_dim,
+                                                 T=T,
+                                                 attn_method=attn_method,
+                                                 attn_head=attn_head,
+                                                 aggr_method=aggr_method,
+                                                 update_method=update_method,
+                                                 activation=activation))
     return
 
 
-# def add_global_pool_to_(node):
-#     """
-#     Function to add operations to graph global pooling variable node
-#     Args:
-#         node: node object
-#
-#     Returns:
-#
-#     """
-#     node.add_op(GlobalMaxPool2())
-#     node.add_op(GlobalAvgPool2())
-#     node.add_op(GlobalSumPool2())
-#     return
-
-
-def add_dense_to_(node):
+def add_global_pooling_to_(node):
     """
     Function to add operations to dense variable node
     Args:
@@ -52,18 +54,17 @@ def add_dense_to_(node):
     Returns:
 
     """
-    node.add_op(Identity())
-
-    activations = [tf.nn.relu, tf.nn.tanh]
-    for units in [16, 32]:
-        for activation in activations:
-            node.add_op(Dense(units=units, activation=activation))
+    # node.add_op(Identity())
+    for functions in [GlobalSumPool, GlobalMaxPool, GlobalAvgPool]:
+        for axis in [-1, -2]:  # Pool in terms of nodes or features
+            node.add_op(functions(axis=axis))
+    node.add_op(Flatten())
     return
 
 
 def create_search_space(input_shape=None,
-                        output_shape=(31, ),
-                        num_gcn_layers=2,
+                        output_shape=(31,),
+                        num_gcn_layers=3,
                         num_dense_layers=1,
                         *args, **kwargs):
     """
@@ -80,27 +81,25 @@ def create_search_space(input_shape=None,
 
     """
     if input_shape is None:
-        input_shape = [(31, 23), (31, 31), (31, 31, 6), (31,)]
-    print(f"================ Create Search Space ================")
+        input_shape = [(31, 23), (31*31, 1), (31*31, 6)]
     arch = KSearchSpace(input_shape, output_shape, regression=True)
-    source = prev_input = arch.input_nodes[0]
-    prev_input1 = arch.input_nodes[1]
-    prev_input2 = arch.input_nodes[2]
-    prev_input3 = arch.input_nodes[3]
+    source = prev_input = arch.input_nodes[0]  # X, node feature matrix (?, 23, 75)
+    prev_input1 = arch.input_nodes[1]  # A, Adjacency matrix (?, 23, 23)
+    prev_input2 = arch.input_nodes[2]  # E
 
     # look over skip connections within a range of the 3 previous nodes
-    anchor_points = collections.deque([source], maxlen=2)
+    anchor_points = collections.deque([source], maxlen=3)
 
     count_gcn_layers = 0
     count_dense_layers = 0
     for _ in range(num_gcn_layers):
-        vnode1 = VariableNode()
-        add_gcn_to_(vnode1)
-        arch.connect(prev_input, vnode1)
-        arch.connect(prev_input1, vnode1)
-        arch.connect(prev_input2, vnode1)
+        graph_attn_cell = VariableNode()
+        add_gat_to_(graph_attn_cell)  # Graph convolution
+        arch.connect(prev_input, graph_attn_cell)  # X --> Graph convolution
+        arch.connect(prev_input1, graph_attn_cell)  # A --> Graph convolution
+        arch.connect(prev_input2, graph_attn_cell)  # E --> Graph convolution
 
-        cell_output = vnode1
+        cell_output = graph_attn_cell
         cmerge = ConstantNode()
         cmerge.set_op(AddByProjecting(arch, [cell_output], activation="relu"))
 
@@ -114,104 +113,26 @@ def create_search_space(input_shape=None,
         anchor_points.append(prev_input)
         count_gcn_layers += 1
 
-    vnode2 = ConstantNode()
-    vnode2.set_op(GlobalMaxPool2())
-    arch.connect(prev_input, vnode2)
+    global_pooling_node = VariableNode()
+    add_global_pooling_to_(global_pooling_node)
+    arch.connect(prev_input, global_pooling_node)  # result from graph conv (?, 23, ?) --> Global pooling (?, 23)
+    prev_input = global_pooling_node
 
-    cell_output = vnode2
-    cmerge = ConstantNode()
-    cmerge.set_op(AddByProjecting(arch, [cell_output], activation="relu"))
-
-    source2 = prev_input = cmerge
-    # look over skip connections within a range of the 3 previous nodes
-    anchor_points2 = collections.deque([source2], maxlen=2)
+    flatten_node = ConstantNode()
+    flatten_node.set_op(Flatten())
+    arch.connect(prev_input, flatten_node)  # result from graph conv (?, 23) --> Flatten
+    prev_input = flatten_node
 
     for _ in range(num_dense_layers):
-        vnode3 = VariableNode()
-        add_dense_to_(vnode3)
-
-        arch.connect(prev_input, vnode3)
-
-        cell_output = vnode3
-        cmerge = ConstantNode()
-        cmerge.set_op(AddByProjecting(arch, [cell_output], activation="relu"))
-
-        for anchor in anchor_points2:
-            skipco = VariableNode()
-            skipco.add_op(Tensor([]))
-            skipco.add_op(Connect(arch, anchor))
-            arch.connect(skipco, cmerge)
-
-        prev_input = cmerge
-        anchor_points2.append(prev_input)
+        dense_node = ConstantNode()
+        dense_node.set_op(Dense(32, activation='relu'))
+        arch.connect(prev_input, dense_node)
+        prev_input = dense_node
         count_dense_layers += 1
 
-    fnode = ConstantNode()
-    fnode.set_op(Flatten())
-    arch.connect(prev_input, fnode)
-    prev_input = fnode
-
-    onode = ConstantNode()
-    onode.set_op(Dense(output_shape[0], activation='relu'))
-    arch.connect(prev_input, onode)
-    prev_input = onode
-
-    mnode = ConstantNode()
-    mnode.set_op(Apply1DMask())
-    arch.connect(prev_input, mnode)
-    arch.connect(prev_input3, mnode)
-
-    print(f"================ Create Search Space Finished w/ {count_gcn_layers} GCN Layers, {count_dense_layers}"
-          f" Dense Layers================")
+    output_node = ConstantNode()
+    output_node.set_op(Dense(output_shape[0], activation='relu'))
+    arch.connect(prev_input, output_node)
 
     return arch
 
-
-def test_create_search_space():
-    """Generate a random neural network from the search_space definition.
-    """
-    from random import random
-    from tensorflow.keras.utils import plot_model
-
-    search_space = create_search_space(input_shape=[(31, 23), (31, 31), (31, 31, 6), (31, )],
-                                       output_shape=(31, ),
-                                       num_gcn_layers=2,
-                                       num_dense_layers=1)
-    ops = [random() for _ in range(search_space.num_nodes)]
-
-    print(f'This search_space needs {len(ops)} choices to generate a neural network.')
-
-    search_space.set_ops(ops)
-
-    model = search_space.create_model()
-    model.summary()
-
-    plot_model(model, to_file='sampled_neural_network.png', show_shapes=True)
-    print("The sampled_neural_network.png file has been generated.")
-    print(f"The size of search space is {search_space.size}")
-    #
-    # from protonation.gnnproton.load_data import load_data
-    # from tensorflow.keras.callbacks import ModelCheckpoint
-    # from tensorflow.keras.optimizers import Adam
-    # ([X_train, A_train, E_train, m_train], y_train), ([X_valid, A_valid, E_valid, m_valid], y_valid) = load_data()
-    # model.compile(loss="mse", optimizer=Adam(learning_rate=1e-3))
-    # mcp = ModelCheckpoint("./1.h5", monitor="val_loss", mode="min", save_best_only=True,
-    #                       save_weights_only=True, verbose=2)
-    # model.fit([X_train, A_train, E_train, m_train], y_train,
-    #          validation_data=([X_valid, A_valid, E_valid, m_valid], y_valid),
-    #          epochs=100, callbacks=[mcp], verbose=0)
-    # model.load_weights("./1.h5")
-    # from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
-    # from tabulate import tabulate
-    # mse_train = mean_squared_error(y_train.ravel(), model.predict([X_train, A_train, E_train, m_train]).ravel())
-    # mae_train = mean_absolute_error(y_train.ravel(), model.predict([X_train, A_train, E_train, m_train]).ravel())
-    # r2_train = r2_score(y_train.ravel(), model.predict([X_train, A_train, E_train, m_train]).ravel())
-    # mse_valid = mean_squared_error(y_valid.ravel(), model.predict([X_valid, A_valid, E_valid, m_valid]).ravel())
-    # mae_valid = mean_absolute_error(y_valid.ravel(), model.predict([X_valid, A_valid, E_valid, m_valid]).ravel())
-    # r2_valid = r2_score(y_valid.ravel(), model.predict([X_valid, A_valid, E_valid, m_valid]).ravel())
-    # print(tabulate([['Train', mse_train, mae_train, r2_train], ['Valid', mse_train, mae_valid, r2_valid]],
-    #                headers=['', 'MSE', 'MAE', 'R2']))
-
-
-if __name__ == '__main__':
-    test_create_search_space()
