210d8823dff0c9b0762f6434270ffab1b99a67ae
Deathn0t <romainegele@gmail.com>
2020-08-07 09:12:58 +0200

    adding test_size, valid_size arguments to covertype load_data function

--- deephyper/benchmark/datasets/covertype.py
+++ deephyper/benchmark/datasets/covertype.py
@@ -6,7 +6,7 @@ from sklearn import datasets
 from sklearn import model_selection
 
 
-def load_data(random_state=42):
+def load_data(random_state=42, test_size=0.1, valid_size=0.1):
     # Random State
     random_state = (
         np.random.RandomState(random_state) if type(random_state) is int else random_state
@@ -15,11 +15,13 @@ def load_data(random_state=42):
     X, y = datasets.fetch_covtype(return_X_y=True)
 
     X_train, X_test, y_train, y_test = model_selection.train_test_split(
-        X, y, test_size=0.33, shuffle=True, random_state=random_state
+        X, y, test_size=test_size, shuffle=True, random_state=random_state
     )
 
+    # relative valid_size on Train set
+    r_valid_size = valid_size / (1.0 - test_size)
     X_train, X_valid, y_train, y_valid = model_selection.train_test_split(
-        X_train, y_train, test_size=0.33, shuffle=True, random_state=random_state
+        X_train, y_train, test_size=r_valid_size, shuffle=True, random_state=random_state
     )
 
     return (X_train, y_train), (X_valid, y_valid), (X_test, y_test)
