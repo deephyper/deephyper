fc6fa01a27ccea4d77902ae371ccd8d0f9c058a0
Deathn0t <romainegele@gmail.com>
2020-08-18 17:46:05 +0200

    WIP ambs update

--- deephyper/evaluator/evaluate.py
+++ deephyper/evaluator/evaluate.py
@@ -320,6 +320,24 @@ class Evaluator:
             return val
 
     def dump_evals(self, saved_key: str = None, saved_keys: list = None):
+        """Dump evaluations to 'results.csv' file.
+
+        If both arguments are set to None, then all keys for all points will be added to the CSV file. Keys are columns and values are used to fill rows.
+
+
+        Args:
+            saved_key (str, optional): If is a key corresponding to an element of points which should be a list. Defaults to None.
+            saved_keys (list|callable, optional): If is a list of key corresponding to elements of points' dictonnaries then it will add them to the CSV file. If callable such as:
+                >>> def saved_keys(self, val: dict):
+                >>>     res = {
+                >>>         "learning_rate": val["hyperparameters"]["learning_rate"],
+                >>>         "batch_size": val["hyperparameters"]["batch_size"],
+                >>>         "ranks_per_node": val["hyperparameters"]["ranks_per_node"],
+                >>>         "arch_seq": str(val["arch_seq"]),
+                >>>     }
+                >>> return res.
+            Then it will add the result to the CSV file. Defaults to None.
+        """
         if not self.finished_evals:
             return
 
