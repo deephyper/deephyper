c1066f43620d79212ca529169595242ceeccba5b
Deathn0t <romainegele@gmail.com>
2020-11-06 15:40:36 +0100

    updated tests for new SpaceFactory interface

---
+++ tests/deephyper/nas/space/factory/dense_skipco_factory.py
@@ -0,0 +1,8 @@
+def test_search_space():
+    from deepspace.tabular import DenseSkipCoFactory
+
+    DenseSkipCoFactory().test()
+
+
+if __name__ == "__main__":
+    test_search_space()
---
+++ tests/deephyper/nas/space/factory/one_layer_factory.py
@@ -0,0 +1,5 @@
+def test_search_space():
+    from deepspace.tabular import OneLayerFactory
+
+    OneLayerFactory().test()
+
