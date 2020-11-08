147e7381a777a0c8435349214ed393032f25ed7e
Deathn0t <romainegele@gmail.com>
2020-11-08 19:42:43 +0100

    setting deepspace to 0.0.3 in setup

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ elif on_gpu:
 
 # What packages are optional?
 EXTRAS = {
-    "tests": ["pytest", "codecov", "pytest-cov", "deepspace>=0.0.2"],
+    "tests": ["pytest", "codecov", "pytest-cov", "deepspace>=0.0.3"],
     "dev": ["twine"],
     "docs": ["Sphinx>=1.8.2", "sphinx_rtd_theme"],
     "analytics": [
@@ -81,7 +81,7 @@ EXTRAS = {
     ],
     "hvd": ["horovod", "mpi4py>=3.0.0"],
     "balsam": ["balsam-flow==0.3.8"],
-    "deepspace": ["deepspace>=0.0.2"],
+    "deepspace": ["deepspace>=0.0.3"],
 }
 
 # The rest you shouldn't have to touch too much :)
