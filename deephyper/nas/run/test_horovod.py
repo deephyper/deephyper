2d1e841e7bf1e287cd36cfa4ce7c14afc100b952
Deathn0t <romainegele@gmail.com>
2021-03-25 14:20:11 +0100

    debug

--- deephyper/nas/run/test_horovod.py
+++ deephyper/nas/run/test_horovod.py
@@ -1,10 +1,18 @@
+import os
+
 import horovod.tensorflow as hvd
+
 
 def run(config: dict) -> float:
     """Using the stateless `run` method, a function can take in any args or kwargs"""
 
     hvd.init()
 
-    print("hvd rank", hvd.rank())
+    print(
+        "hvd rank: ",
+        hvd.rank(),
+        " - CUDA_VISIBLE: ",
+        os.environ.get("CUDA_VISIBLE_DEVICES"),
+    )
 
     return 0
