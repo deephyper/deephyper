01e1b01bc4e971c7e15e2b064fbc8776842dd69e
Deathn0t <romainegele@gmail.com>
2021-03-29 11:52:23 +0200

    update

--- deephyper/core/cli/ray_submit.py
+++ deephyper/core/cli/ray_submit.py
@@ -80,10 +80,12 @@ def add_subparser(subparsers):
 def generate_other_arguments(func, **kwargs):
     cl_format = ""
     for k,v in kwargs.items():
-        arg = "--" + "-".join(k.split("_"))
-        val = str(v)
-        arg_val = f"{arg}={val} "
-        cl_format += arg_val
+        if v is not None:
+            arg = "--" + "-".join(k.split("_"))
+            val = str(v)
+            arg_val = f"{arg}={val} "
+            cl_format += arg_val
+    cl_format = cl_format[:-1]
     return cl_format
 
 
