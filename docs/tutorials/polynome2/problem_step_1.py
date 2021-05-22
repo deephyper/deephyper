abb6408e0d30dc331daeaf88bafd6178befc927f
felixeperez <33436614+felixeperez@users.noreply.github.com>
2021-05-22 13:04:17 -0500

    Updated activation dimension option for the problem.

--- docs/tutorials/polynome2/problem_step_1.py
+++ docs/tutorials/polynome2/problem_step_1.py
@@ -3,10 +3,10 @@ from deephyper.problem import HpProblem
 Problem = HpProblem()
 
 Problem.add_dim("units", (1, 100))
-Problem.add_dim("activation", [None, "relu", "sigmoid", "tanh"])
+Problem.add_dim("activation", ["N/A", "relu", "sigmoid", "tanh"])
 Problem.add_dim("lr", (0.0001, 1.0))
 
-Problem.add_starting_point(units=10, activation=None, lr=0.01)
+Problem.add_starting_point(units=10, activation="N/A", lr=0.01)
 
 if __name__ == "__main__":
     print(Problem)
