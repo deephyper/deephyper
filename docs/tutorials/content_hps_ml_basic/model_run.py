b992c7bee2fc4edebdaa0d6aa1a40f14eee34d68
Deathn0t <romainegele@gmail.com>
2021-06-30 09:17:13 +0200

    resolved bug in basic ml because id is now being passed in config

--- docs/tutorials/content_hps_ml_basic/model_run.py
+++ docs/tutorials/content_hps_ml_basic/model_run.py
@@ -10,7 +10,14 @@ def run(config):
 
     (X, y), (vX, vy) = load_data()
 
-    classifier = RandomForestClassifier(n_jobs=8, random_state=rs, **config)
+    classifier = RandomForestClassifier(
+        n_jobs=8,
+        random_state=rs,
+        n_estimators=config["n_estimators"],
+        criterion=config["criterion"],
+        max_depth=config["max_depth"],
+        min_samples_split=config["min_samples_split"]
+    )
     classifier.fit(X, y)
 
     mean_accuracy = classifier.score(vX, vy)
