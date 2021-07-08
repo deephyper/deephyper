cb800407afa35062045b2b3385d463f2703c2a40
Deathn0t <romainegele@gmail.com>
2021-07-08 14:52:12 +0200

    debug balsam-submit

--- deephyper/core/cli/balsam_submit.py
+++ deephyper/core/cli/balsam_submit.py
@@ -173,10 +173,10 @@ def setup_job(job, problem, run, **kwargs):
     if run:
         job.args += f" --run {run}"
 
-    invalid_keys = ["time_minutes", "nodes", "queue", "project", "job_mode", "func"]
+    invalid_keys = ["time_minutes", "nodes", "queue", "project", "job_mode"]
     for k in invalid_keys:
         kwargs.pop(k)
-    args = generate_other_arguments(**kwargs)
+    args = generate_other_arguments(func=None, **kwargs)
     if len(args) > 0:
         job.args += f" {args}"
 
