dfc39b0801175583ed9caf2520f3c9fed080a57f
Deathn0t <romainegele@gmail.com>
2021-09-27 18:05:12 +0200

    added LoggerCallback in evaluator.callback

--- deephyper/evaluator/callback.py
+++ deephyper/evaluator/callback.py
@@ -30,3 +30,18 @@ class ProfilingCallback(Callback):
         cols = ["timestamp", "n_jobs_running"]
         df = pd.DataFrame(self.data, columns=cols)
         return df
+
+
+class LoggerCallback(Callback):
+
+    def __init__(self):
+        self._best_objective = None
+        self._n_done = 0
+
+    def on_done(self, job):
+        self._n_done += 1
+        if self._best_objective is None:
+            self._best_objective = job.result
+        else:
+            self._best_objective = max(job.result, self._best_objective)
+        print(f"[{self._n_done:05d}] -- best objective: {self._best_objective} -- received objective: {job.result}")
