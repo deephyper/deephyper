93ad5d890d2fc5137e65669942d4b3c7a43629e0
minesweeter <joceran.g@gmail.com>
2021-11-04 17:35:15 +0400

    Benchmark criterias selection

--- deephyper/core/analytics/dashboard/_streamlit_app.py
+++ deephyper/core/analytics/dashboard/_streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+from tinydb import TinyDB, Query
+import json
 
 from deephyper.core.analytics.dashboard._pyplot import plot_single_line
 
@@ -99,6 +101,126 @@ def _files_selection():
 def _database_selection():
     uploaded_file = st.sidebar.file_uploader("Choose a Database file")
 
+    if uploaded_file is not None:
+        data = json.load(uploaded_file)
+        with open("db.json", 'w') as outfile:
+            json.dump(data, outfile)
+        db = TinyDB("db.json")
+
+        # Récup les caractéristiques de la db que l’on peut sélectionner
+        selection = db.all()
+        criterias = {}
+        not_criterias = ["description", "results"]
+        for select in selection:
+            _get_criterias(select, criterias, not_criterias)
+
+        # Afficher les choix possibles
+        choices = {}
+        with st.sidebar.expander("Select benchmark criterias :"):
+            _select_criterias(criterias, choices)
+
+        # Faire la sélection
+        run = Query()
+        run = _test_criterias(choices, run)
+        selection = db.search(run)
+        
+        # Print les caractéristiques de la selection
+        # Moyenner les valeurs de la sélection
+        # Moyenner les courbes de la sélection
+        # Afficher les valeurs
+        # Afficher les courbes
+
+        st.header("Benchmark selection")
+        # Headers
+        columns = st.columns(len(selection))
+        for i in range(len(selection)):
+            with columns[i]:
+                st.subheader(f"Benchmark {i+1}")
+                bench = selection[i]
+                for part, elements in bench.items():
+                    if part != "results":
+                        st.markdown(f"- **{part} :**")
+                        for key, value in elements.items():
+                            if key != "stable":
+                                st.markdown(f"*{key}* : {value}")
+
+        st.header("Results")
+
+        # results
+        for result in selection[0]["results"]:
+            if type(selection[0]["results"][result]) != dict:
+                st.markdown(f"**{result} :**")
+                for i in range(len(selection)):
+                    value = selection[i]["results"][result]
+                    st.markdown(f"**- benchmark {i+1}** : {value}")
+
+        # 1st figure: number of jobs vs time
+        fig = plt.figure()
+        for i in range(len(selection)):
+            profile = pd.DataFrame.from_dict(selection[i]["results"]["profile"])
+            plt.step(profile.timestamp, profile.n_jobs_running, where="post", label=f"benchmark {i+1}")
+        plt.legend()
+        plt.title("profile")
+        st.pyplot(fig)
+        plt.close()
+
+        # 2nd figure: objective vs iter
+        def to_max(l):
+            r = [l[0]]
+            for e in l[1:]:
+                r.append(max(r[-1], e))
+            return r
+
+        fig = plt.figure()
+        for i in range(len(selection)):
+            plt.plot(to_max(pd.DataFrame.from_dict(selection[i]["results"]["search"]).objective), label=f"benchmark {i+1}")
+        plt.legend()
+        plt.title("search")
+        st.pyplot(fig)
+        plt.close()
+
+def _get_criterias(data, criterias, not_criterias):
+    for key, val in data.items():
+        if key not in not_criterias:
+            if type(val) == list:
+                val = tuple(val)
+            if key not in criterias:
+                if type(val) == dict:
+                    criterias[key] = {}
+                else:
+                    criterias[key] = {val}
+            if type(val) == dict :
+                _get_criterias(data[key], criterias[key], not_criterias)
+            else:
+                criterias[key].add(val)
+
+def _select_criterias(criterias, choices):
+    for key, val in criterias.items():
+        if type(val) == dict :
+            st.markdown("------")
+            st.write(f"{key} :")
+            choices[key] = {}
+            _select_criterias(criterias[key], choices[key])
+        else:
+            choice = st.multiselect(
+                label=key,
+                options=val
+            )
+            if len(choice) != 0 and type(choice[0]) == tuple:
+                choice = list(map(lambda x: list(x), choice))
+            choices[key] = choice
+
+def _test_criterias(choices, query):
+    sentence = query.noop()
+    for key, val in choices.items():
+        if type(val) == dict :
+            test = _test_criterias(choices[key], query[key])
+        elif len(val) != 0:
+            test = query[key].one_of(val)
+        else:
+            test = query.noop()
+        sentence = (~ (query[key].exists()) | test) & sentence
+    return sentence
 
 def main():
     st.title("DeepHyper Dashboard")
