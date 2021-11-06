14f26268896df5519fcc9933c0d90abb89f7f7f9
minesweeter <joceran.g@gmail.com>
2021-11-06 22:07:37 +0400

    Better display and modularity

--- deephyper/core/analytics/dashboard/_results_processors.py
+++ deephyper/core/analytics/dashboard/_results_processors.py
@@ -1,71 +1,213 @@
-from matplotlib import pyplot as plt
+import statistics as stat
+
 import numpy as np
 import pandas as pd
+import streamlit as st
+from matplotlib import pyplot as plt
+from numpy.core.numeric import NaN
 
 
 class ProfileProcessor():
-    def preprocess(val, roll_val):
-        profile = pd.DataFrame({"n_jobs_running" : val["n_jobs_running"]}, index=val["timestamp"])
-        profile.index -= profile.index[0]
-        new_base = np.arange(0, val["timestamp"][-1], 0.1)
-        profile = profile.reindex(profile.index.union(new_base)).interpolate('values').loc[new_base]
-        profile = profile.rolling(roll_val).mean()
-        return profile.to_dict()
-    
-    def fuse(val_list):
-        df_list = list(map(lambda x: pd.DataFrame(x), val_list))
+    def __init__(self, menu: st.expander):
+        self.warnings = []
+        menu.markdown("**Evolution of Worker Use:**")
+        self.roll_val = int(menu.slider(
+            'Roll value (in s.)', .1, 25., 12.5)*10)
+
+    def verify(self, idx, key, val):
+        if "n_jobs_running" not in val.keys() or "timestamp" not in val.keys():
+            self.warnings.append(
+                f"config {idx+1} : a run is missing 'n_jobs_running' or 'timestamp' in {key}.")
+            val = None
+        return val
+
+    def preprocess(self, val):
+        if val is not None:
+            profile = pd.DataFrame(
+                {"n_jobs_running": val["n_jobs_running"]}, index=val["timestamp"])
+            profile.index -= profile.index[0]
+            new_base = np.arange(0, val["timestamp"][-1], 0.1)
+            profile = profile.reindex(profile.index.union(
+                new_base)).interpolate('values').loc[new_base]
+            profile = profile.rolling(self.roll_val).mean()
+        else:
+            profile = pd.DataFrame({"n_jobs_running": 0}, index=[0])
+        return profile
+
+    def fuse(self, df_list):
         df_concat = pd.concat(df_list)
         by_row_index = df_concat.groupby(df_concat.index)
-        df = by_row_index.mean()
-        return df.to_dict()
-    
-    def display(st, val_list):
+        df_mean = by_row_index.mean()
+        df_max = by_row_index.max()
+        df_min = by_row_index.min()
+        df_std = by_row_index.std()
+        return (df_mean, df_max, df_min, df_std)
+
+    def display(self, st, key, df_list, colors):
+        st.subheader("Evolution of Worker Use:")
+        for warning in self.warnings:
+            st.warning(warning)
         fig = plt.figure()
-        for i in range(len(val_list)):
-            if val_list[i] is not None:
-                df = pd.DataFrame.from_dict(val_list[i])
-            plt.plot(df)
+        for i in range(len(df_list)):
+            if df_list[i] is not None:
+                df_mean, df_max, df_min, df_std = df_list[i]
+                plt.plot(df_mean, label=f"config {i+1}", color=colors[i])
+                plt.fill_between(df_mean.index, df_min.n_jobs_running,
+                                 df_max.n_jobs_running, alpha=.1, color=colors[i])
+                plt.fill_between(df_mean.index, df_mean.n_jobs_running-df_std.n_jobs_running,
+                                 df_mean.n_jobs_running+df_std.n_jobs_running, alpha=.2, color=colors[i])
+        plt.xlabel("Time (sec.)")
+        plt.ylabel("Number of Used Workers")
+        plt.legend()
+        plt.grid()
+        plt.tight_layout()
         st.pyplot(fig)
+        return True
+
 
 class SearchProcessor():
-    def preprocess(val):
+    def __init__(self):
+        self.warnings = []
+
+    def verify(self, idx, key, val):
+        if "objective" not in val.keys():
+            self.warnings.append(
+                f"config {idx+1} : a run is missing 'objective' in {key}.")
+            val = None
+        return val
+
+    def preprocess(self, val):
         def to_max(l):
             r = [l[0]]
             for e in l[1:]:
                 r.append(max(r[-1], e))
             return r
+        if val is not None:
+            objective = val["objective"]
+            search = pd.DataFrame({"objective": to_max(objective)})
+        else:
+            search = pd.DataFrame()
+        return search
 
-        objective = val["objective"]
-        search = pd.DataFrame({"objective" : to_max(objective)})
-        return search.to_dict()
-    
-    def fuse(val_list):
-        df_list = list(map(lambda x: pd.DataFrame(x), val_list))
+    def fuse(self, df_list):
         df_concat = pd.concat(df_list)
         by_row_index = df_concat.groupby(df_concat.index)
-        df = by_row_index.mean()
-        return df.to_dict()
-    
-    def display(st, val_list):
-        fig = plt.figure()
-        for i in range(len(val_list)):
-            if val_list[i] is not None:
-                df = pd.DataFrame.from_dict(val_list[i])
-            plt.plot(df)
-        st.pyplot(fig)
+        df_mean = by_row_index.mean()
+        df_max = by_row_index.max()
+        df_min = by_row_index.min()
+        df_std = by_row_index.std()
+        return (df_mean, df_max, df_min, df_std)
 
-class DefaultProcessor():
-    def preprocess(val):
+    def display(self, st, key, df_list, colors):
+        st.subheader("Objective Evolution:")
+        for warning in self.warnings:
+            st.warning(warning)
+        fig = plt.figure()
+        for i in range(len(df_list)):
+            if df_list[i] is not None:
+                df_mean, df_max, df_min, df_std = df_list[i]
+                plt.plot(df_mean, label=f"config {i+1}", color=colors[i])
+                plt.fill_between(df_mean.index, df_min.objective,
+                                 df_max.objective, alpha=.1, color=colors[i])
+                plt.fill_between(df_mean.index, df_mean.objective-df_std.objective,
+                                 df_mean.objective+df_std.objective, alpha=.2, color=colors[i])
+        plt.xlabel("Iteration")
+        plt.ylabel("Objective")
+        plt.legend()
+        plt.grid()
+        plt.tight_layout()
+        st.pyplot(fig)
+        return True
+
+
+class PercUtilProcessor():
+    def __init__(self):
+        self.warnings = []
+
+    def verify(self, idx, key, val):
+        if type(val) not in [int, float]:
+            self.warnings.append(f"config {idx+1}: '{key}' is not numerical.")
+            val = 0
         return val
 
-    def fuse(val_list):
-        if type(val_list[0]) in [int, float]:
-            avrg = sum(val_list)/len(val_list)
-            return avrg
-        else:
-            return val_list[0]
-    
-    def display(st, val_list):
-        for i in range(len(val_list)):
+    def preprocess(self, val):
+        return val
+
+    def fuse(self, val_list):
+        avrg = stat.mean(val_list)
+        try:
+            std = stat.stdev(val_list)
+        except:
+            std = 0
+        return (avrg, std)
+
+    def display(self, st, key, val_list, colors):
+        st.subheader("Worker Use:")
+        for warning in self.warnings:
+            st.warning(warning)
+        fig = plt.figure()
+        p = -1
+        for i in reversed(range(len(val_list))):
             if val_list[i] is not None:
-                st.markdown(f"**Config {i+1} :** {val_list[i]}")
+                p += 1
+                avrg, std = val_list[i]
+                avrg *= 100
+                std *= 100
+                err_color = colors[i].copy()
+                color = colors[i]
+                color[-1] = 0.7
+                text_color = 'white' if color[:-1].sum() < 1.5 else '0.2'
+                plt.barh(f"config {i+1}", avrg, xerr=std,
+                         color=color, ecolor=err_color)
+                plt.barh(f"config {i+1}", 100-avrg,
+                         left=avrg, color='lightgrey')
+                plt.text(avrg/2, p, f"{round(avrg, 2)}%\nUsed",
+                         ha='center', va='center', color=text_color)
+                plt.text(avrg/2+50, p, f"{round(100-avrg, 2)}%\nUnused",
+                         ha='center', va='center', color='0.2')
+
+        plt.xlabel("Percentage")
+        plt.tight_layout()
+        st.pyplot(fig)
+        return True
+
+
+class DefaultProcessor():
+
+    def __init__(self, menu: st.expander):
+        self.warnings = []
+        menu.markdown("**Other Results:**")
+        fuser_choice = menu.radio(
+            "What value should be computed over the same config ?",
+            ('mean', 'median', 'max', 'min'))
+        fusers = {
+            'mean': stat.mean,
+            'median': stat.median,
+            'max': max,
+            'min': min
+        }
+        self.fuser = fusers.get(fuser_choice, stat.mean)
+
+    def verify(self, idx, key, val):
+        if type(val) not in [int, float]:
+            self.warnings.append(f"config {idx+1}: '{key}' is not numerical.")
+            val = NaN
+        return val
+
+    def preprocess(self, val):
+        return val
+
+    def fuse(self, val_list):
+        avrg = self.fuser(val_list)
+        return round(avrg, 4)
+
+    def display(self, st, key, val_list, colors):
+        return False
+
+    def display_raw(self, st, raw_data):
+        st.subheader("Other Results:")
+        for warning in self.warnings:
+            self.st.warning(warning)
+        df = pd.DataFrame(raw_data, index=[
+                          f"config {i+1}" for i in range(len(list(raw_data.values())[0]))])
+        st.dataframe(df)
--- deephyper/core/analytics/dashboard/_streamlit_app.py
+++ deephyper/core/analytics/dashboard/_streamlit_app.py
@@ -1,13 +1,16 @@
-from numpy.core.numeric import identity
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-from tinydb import TinyDB, Query
 import json
 import tempfile
 
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import streamlit as st
 from deephyper.core.analytics.dashboard._pyplot import plot_single_line
-from deephyper.core.analytics.dashboard._results_processors import ProfileProcessor, SearchProcessor, DefaultProcessor
+from deephyper.core.analytics.dashboard._results_processors import (
+    DefaultProcessor, PercUtilProcessor, ProfileProcessor, SearchProcessor)
+from matplotlib.pylab import cm
+from numpy.core.numeric import identity
+from tinydb import Query, TinyDB
 
 
 def _worker_utilization(profile, num_workers):
@@ -26,154 +29,158 @@ def _worker_utilization(profile, num_workers):
     return perc_util * 100
 
 
-def _files_selection():
-    uploaded_file = st.sidebar.file_uploader("Choose a CSV file")
+def _files_selection(uploaded_file):
+    # Can be used wherever a "file-like" object is accepted:
+    df = pd.read_csv(uploaded_file, index_col=0)
 
-    if uploaded_file is not None:
+    # assuming it's a search csv
+    if (
+        "objective" in df.columns
+        and "elapsed_sec" in df.columns
+        and "duration" in df.columns
+        and "id" in df.columns
+    ):
+        df["iteration"] = df.index
 
-        # Can be used wherever a "file-like" object is accepted:
-        df = pd.read_csv(uploaded_file, index_col=0)
+        st.header("Line Plot")
+        st.sidebar.header("Line Plot")
+        line_plot_option_x = st.sidebar.selectbox(
+            "Choose the X-axis",
+            tuple(df.columns),
+            index=list(df.columns).index("iteration"),
+        )
+        line_plot_option_y = st.sidebar.selectbox(
+            "Choose the Y-axis",
+            tuple(df.columns),
+            index=list(df.columns).index("objective"),
+        )
 
-        # assuming it's a search csv
-        if (
-            "objective" in df.columns
-            and "elapsed_sec" in df.columns
-            and "duration" in df.columns
-            and "id" in df.columns
-        ):
-            df["iteration"] = df.index
+        plot_single_line(df, line_plot_option_x, line_plot_option_y)
 
-            st.header("Line Plot")
-            st.sidebar.header("Line Plot")
-            line_plot_option_x = st.sidebar.selectbox(
-                "Choose the X-axis",
-                tuple(df.columns),
-                index=list(df.columns).index("iteration"),
-            )
-            line_plot_option_y = st.sidebar.selectbox(
-                "Choose the Y-axis",
-                tuple(df.columns),
-                index=list(df.columns).index("objective"),
-            )
+    # assuming it is a profile csv
+    elif "timestamp" in df.columns and "n_jobs_running" in df.columns:
+        st.header("Worker Utilization")
+        num_workers = st.number_input(
+            "Number of Workers", value=df.n_jobs_running.max()
+        )
 
-            plot_single_line(df, line_plot_option_x, line_plot_option_y)
+        perc_ut = _worker_utilization(df, num_workers)
 
-        # assuming it is a profile csv
-        elif "timestamp" in df.columns and "n_jobs_running" in df.columns:
-            st.header("Worker Utilization")
-            num_workers = st.number_input(
-                "Number of Workers", value=df.n_jobs_running.max()
-            )
+        fig = plt.figure()
+        plt.pie(
+            [perc_ut, 100 - perc_ut],
+            explode=(0.1, 0),
+            labels=["Used", "Not Used"],
+            autopct="%1.1f%%",
+            shadow=True,
+            startangle=90,
+        )
+        st.pyplot(fig)
+        plt.close()
 
-            perc_ut = _worker_utilization(df, num_workers)
+        t0 = df.iloc[0].timestamp
+        df["timestamp"] = df.timestamp - t0
 
-            fig = plt.figure()
-            plt.pie(
-                [perc_ut, 100 - perc_ut],
-                explode=(0.1, 0),
-                labels=["Used", "Not Used"],
-                autopct="%1.1f%%",
-                shadow=True,
-                startangle=90,
-            )
-            st.pyplot(fig)
-            plt.close()
+        t0 = float(df.iloc[0].timestamp)
+        t_max = float(df.iloc[-1].timestamp)
 
-            t0 = df.iloc[0].timestamp
-            df["timestamp"] = df.timestamp - t0
+        t0, t_max = st.slider(
+            "Time Range", min_value=t0, max_value=t_max, value=(t0, t_max)
+        )
 
-            t0 = float(df.iloc[0].timestamp)
-            t_max = float(df.iloc[-1].timestamp)
+        df = df[(df.timestamp >= t0) & (df.timestamp <= t_max)]
+        fig = plt.figure()
+        plt.step(df.timestamp, df.n_jobs_running, where="post")
+        plt.xlabel("Time (sec.)")
+        plt.ylabel("Number of Used Workers")
+        st.pyplot(fig)
+        plt.close()
 
-            t0, t_max = st.slider(
-                "Time Range", min_value=t0, max_value=t_max, value=(t0, t_max)
-            )
+    else:
+        st.write("Sorry but this type of CSV is not supported!")
 
-            df = df[(df.timestamp >= t0) & (df.timestamp <= t_max)]
-            fig = plt.figure()
-            plt.step(df.timestamp, df.n_jobs_running, where="post")
-            plt.xlabel("Time (sec.)")
-            plt.ylabel("Number of Used Workers")
-            st.pyplot(fig)
-            plt.close()
 
-        else:
-            st.write("Sorry but this type of CSV is not supported!")
+def _database_selection(uploaded_file):
+    temp_db = tempfile.NamedTemporaryFile(mode="w+")
+    json.dump(json.load(uploaded_file), temp_db)
+    temp_db.read()
+    db = TinyDB(temp_db.name)
 
-def _database_selection():
-    uploaded_file = st.sidebar.file_uploader("Choose a Database file")
+    # Get the selectable caracteristics from the database
+    selection = db.all()
+    criterias = {}
+    not_criterias = ["description", "results", "date"]
+    for select in selection:
+        criterias = _get_criterias(select, criterias, not_criterias)
 
-    if uploaded_file is not None:
-        temp_db = tempfile.NamedTemporaryFile(mode="w+")
-        json.dump(json.load(uploaded_file), temp_db)
-        temp_db.read()
-        db = TinyDB(temp_db.name)
+    # Show the possible criterias
+    with st.sidebar.expander("Select benchmark criterias :"):
+        choices = _select_choices(criterias, 1)
 
-        # Get the selectable caracteristics from the database
-        selection = db.all()
-        criterias = {}
-        not_criterias = ["description", "results", "date"]
-        for select in selection:
-            criterias = _get_criterias(select, criterias, not_criterias)
+    # Select the corresponding runs
+    query = _generate_query(choices, Query())
+    selection = db.search(query)
+    select_size = len(selection)
+    if select_size != 0:
+        st.sidebar.info("{nb} benchmark{s} found.".format(
+            nb=select_size, s='s' if select_size > 1 else ''))
 
-        # Show the possible criterias
-        with st.sidebar.expander("Select benchmark criterias :"):
-            choices = _select_choices(criterias, 1)
+    group = st.sidebar.checkbox(
+        "Group identical runs in the same config", True)
+    # Sort the selection and bring together identical runs
+    configurations, bench_per_conf, results_list = _regroup_identicals(
+        selection, group)
 
-        # Select the corresponding runs
-        query = _generate_query(choices, Query())
-        selection = db.search(query)
-        select_size = len(selection)
-        if select_size != 0:
-            st.sidebar.info("{nb} benchmark{s} found.".format(nb=select_size, s='s' if select_size > 1 else ''))
-        
-        # Sort the selection and bring together identical runs
-        configurations, bench_per_conf, results_list = _regroup_identicals(selection)
-
-        # Show configurations info
+    # Show configurations info
+    if len(configurations) == 0:
+        st.warning("0 benchmark found.")
+    else:
         st.header("Selected configurations")
-        for i in range(len(configurations)):
-            with st.expander(f"Configuration {i+1}"):
-                st.info(f"Number of benchmarks comprised : {bench_per_conf[i]}")
-                _show_bench_info(configurations[i], 1)
+    colors = plt.get_cmap('gnuplot')(
+        np.linspace(0.1, 0.80, len(configurations)))
+    for i in range(len(configurations)):
+        with st.expander(f"Configuration {i+1}"):
+            st.info(f"Number of benchmarks comprised : {bench_per_conf[i]}")
+            _show_bench_info(configurations[i], 1, colors[i])
 
-        # Process the results
-        results_processors = {
-            "profile": ProfileProcessor,
-            "search": SearchProcessor
-        }
-        
-        roll_val = st.sidebar.slider('Roll value', 1, 50, 25)
-        preprocess_params = {
-            "profile": {roll_val}
-        }
-        for results in results_list:
-            for key, val_list in results.items():
-                processor = results_processors.get(key, DefaultProcessor)
+    # Process the results
+    menu = st.sidebar.expander("Display Parameters :")
+    results_processors = {
+        "profile": ProfileProcessor(menu),
+        "search": SearchProcessor(),
+        "perc_util": PercUtilProcessor()
+    }
+    default_processor = DefaultProcessor(menu)
 
-                # Preprocess the results
-                params = preprocess_params.get(key, {})
-                new_val_list = []
-                for val in val_list:
-                    new_val = processor.preprocess(val, *params)
-                    new_val_list.append(new_val)
+    for idx, results in enumerate(results_list):
+        for key, val_list in results.items():
+            processor = results_processors.get(key, default_processor)
 
-                # Fuse the results of each configuration
-                fused_val = processor.fuse(new_val_list)
-                results[key] = fused_val
+            # Preprocess the results
+            new_val_list = []
+            for val in val_list:
+                val = processor.verify(idx, key, val)
+                new_val = processor.preprocess(val)
+                new_val_list.append(new_val)
 
-        # Rearrange to regroup common results together
-        report = _regroup_results(results_list)
+            # Fuse the results of each configuration
+            fused_val = processor.fuse(new_val_list)
+            results[key] = fused_val
 
-        # Show the results
+    # Rearrange to regroup common results together
+    report = _regroup_results(results_list)
+
+    # Show the results
+    if len(configurations) != 0:
         st.header("Results")
-        if len(configurations) == 0:
-            st.warning("0 benchmark found.")
 
-        for key, val_list in report.items():
-            st.subheader(f"{key} :")
-            processor = results_processors.get(key, DefaultProcessor)
-            processor.display(st, val_list)
+    raw_data = {}
+    for key, val_list in report.items():
+        processor = results_processors.get(key, default_processor)
+        display = processor.display(st, key, val_list, colors)
+        if not display:
+            raw_data[key] = val_list
+    default_processor.display_raw(st, raw_data)
 
 
 def _get_criterias(data, old_criterias, not_criterias):
@@ -187,35 +194,42 @@ def _get_criterias(data, old_criterias, not_criterias):
                     criterias[key] = {}
                 else:
                     criterias[key] = {val}
-            if type(val) == dict :
-                criterias[key] = _get_criterias(data[key], criterias[key], not_criterias)
+            if type(val) == dict:
+                criterias[key] = _get_criterias(
+                    data[key], criterias[key], not_criterias)
             else:
                 criterias[key].add(val)
     return criterias
 
+
 def _select_choices(criterias, depth):
     choices = {}
     for key, val in criterias.items():
-        if type(val) == dict :
+        if type(val) == dict:
             if depth == 1:
                 st.markdown("------")
             h = depth+1 if depth < 6 else 6
             st.markdown(f"{(h)*'#'} {key} :")
             choices[key] = _select_choices(criterias[key], depth+1)
         else:
-            choice = st.multiselect(
-                label=key,
-                options=val
-            )
+            if len(val) == 1:
+                choice = list(val)
+                st.markdown(f"{key} : {choice[0]}")
+            else:
+                choice = st.multiselect(
+                    label=key,
+                    options=val
+                )
             if len(choice) != 0 and type(choice[0]) == tuple:
                 choice = list(map(lambda x: list(x), choice))
             choices[key] = choice
     return choices
 
+
 def _generate_query(choices, query):
     sentence = query.noop()
     for key, val in choices.items():
-        if type(val) == dict :
+        if type(val) == dict:
             test = _generate_query(choices[key], query[key])
         elif len(val) != 0:
             test = query[key].one_of(val)
@@ -224,43 +238,51 @@ def _generate_query(choices, query):
         sentence = (~ (query[key].exists()) | test) & sentence
     return sentence
 
-def _show_bench_info(configuration, depth):
+
+def _show_bench_info(configuration, depth, c):
     for key, val in configuration.items():
         h = depth+2 if depth < 4 else 6
-        if type(val) == dict :
+        if type(val) == dict:
             if depth == 1:
                 st.markdown("------")
-            st.markdown(f"{h*'#'} {key}:")
-            _show_bench_info(configuration[key], depth+1)
+                color = f"rgb({c[0]*255}, {c[1]*255}, {c[2]*255})"
+                st.markdown(
+                    f"<h3 style='color:{color}'>{key} :</h3>", unsafe_allow_html=True)
+            else:
+                st.markdown(f"{h*'#'} {key}:")
+            _show_bench_info(configuration[key], depth+1, c)
         else:
             st.markdown(f"**{key}:** {val}")
 
-def _regroup_identicals(select):
+
+def _regroup_identicals(select, group):
     selection = select.copy()
+
     def _cleanup_info(info):
         try:
             info["summary"].pop("date")
         except:
             pass
-    
+
     configurations = []
     bench_per_conf = []
     results_list = []
     for s in selection:
         results = s.pop("results")
         _cleanup_info(s)
-        try:
+        if s in configurations and group:
             index = configurations.index(s)
             bench_per_conf[index] += 1
             for key, val in results.items():
                 results_list[index][key].append(val)
-        except:
+        else:
             configurations.append(s)
             bench_per_conf.append(1)
             for key, val in results.items():
                 results[key] = [val]
             results_list.append(results)
     return configurations, bench_per_conf, results_list
+
 
 def _regroup_results(results_list):
     report = {}
@@ -278,14 +300,32 @@ def _regroup_results(results_list):
 
 def main():
     st.title("DeepHyper Dashboard")
+    st.sidebar.markdown("**Import a file**")
+    upload_type = st.sidebar.radio("Upload type :", ("Import File", "Path"))
 
-    dashboard_type = st.sidebar.radio(
-        "Where are your data stored?", ("Files", "Database")
-    )
-    if dashboard_type == "Files":
-        _files_selection()
+    if upload_type == "Import File":
+        uploaded_file = st.sidebar.file_uploader("")
     else:
-        _database_selection()
+        path = st.sidebar.text_input("Enter the file path:")
+        try:
+            uploaded_file = open(path)
+        except:
+            st.sidebar.warning("No such file.")
+            uploaded_file = None
+
+    if uploaded_file is not None:
+        ext = uploaded_file.name.split('.')[-1]
+
+        boards = {
+            "csv": _files_selection,
+            "json": _database_selection
+        }
+
+        def default(x): return st.sidebar.warning(
+            f"File should either be a csv or a json, not {ext}.")
+        boards.get(ext, default)(uploaded_file)
+    else:
+        st.sidebar.info("Choose either a CSV or a JSON file.")
 
 
 if __name__ == "__main__":
