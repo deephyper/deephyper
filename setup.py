da6ec2c92ed8e97982bed9d6ba0421099e295d0f
Deathn0t <romainegele@gmail.com>
2021-11-17 19:13:58 +0100

    adding refactoring of setup

--- setup.py
+++ setup.py
@@ -4,41 +4,18 @@
 # Note: To use the 'upload' functionality of this file, you must:
 #   $ pip install twine
 
-import io
 import os
 import sys
 from shutil import rmtree
 
-from setuptools import find_packages, setup, Command
+from setuptools import Command, setup
 
-on_rtd = os.environ.get("READTHEDOCS") == "True"
-
-# Package meta-data.
-NAME = "deephyper"
-DESCRIPTION = "Scalable asynchronous neural architecture and hyperparameter search for deep neural networks."
-URL = "https://github.com/deephyper/deephyper"
-REQUIRES_PYTHON = ">=3.7, <3.10"
-VERSION = None
-
-# Build Author list
-authors = {
-    "Prasanna Balaprakash": "pbalapra@anl.gov",
-    "Romain Egele": "romain.egele@polytechnique.edu",
-    "Misha Salim": "msalim@anl.gov",
-    "Romit Maulik": "rmaulik@anl.gov",
-    "Venkat Vishwanath": "venkat@anl.gov",
-    "Stefan Wild": "wild@anl.gov",
-}
-AUTHOR = ""
-for i, (k, v) in enumerate(authors.items()):
-    if i > 0:
-        AUTHOR += ", "
-    AUTHOR += f"{k} <{v}>"
+# on_rtd = os.environ.get("READTHEDOCS") == "True"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    "tensorflow>=2.0.0",
-    "tensorflow_probability",
+    # "tensorflow>=2.0.0",
+    # "tensorflow_probability",
     "numpy",  # ==1.19.4",  # working with 1.20.1
     "dh-scikit-optimize==0.9.4",
     "scikit-learn>=0.23.1",
@@ -54,7 +31,7 @@ REQUIRED = [
     "xgboost",
     "openml==0.10.2",
     "matplotlib>=3.0.3",
-    "statsmodels",
+    # "statsmodels",
 ]
 
 # What packages are optional?
@@ -96,22 +73,6 @@ EXTRAS = {
 # If you do change the License, remember to change the Trove Classifier for that!
 
 here = os.path.abspath(os.path.dirname(__file__))
-
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
-try:
-    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
-        long_description = "\n" + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    with open(os.path.join(here, NAME, "__version__.py")) as f:
-        exec(f.read(), about)
-else:
-    about["__version__"] = VERSION
 
 
 class UploadCommand(Command):
@@ -206,45 +167,12 @@ class TestInstallCommand(Command):
 
 # Where the magic happens:
 setup(
-    name=NAME,
-    version=about["__version__"],
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author=AUTHOR,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    project_urls={
-        "Documentation": "https://deephyper.readthedocs.io/",
-        "Source": "https://github.com/deephyper/deephyper",
-        "Tracker": "https://github.com/deephyper/deephyper/issues",
-    },
-    packages=find_packages(exclude=("tests",)),
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['deephyper'],
     install_requires=REQUIRED,
     extras_require=EXTRAS,
-    include_package_data=True,
-    license="ANL",
-    classifiers=[
-        # Trove classifiers
-        # https://pypi.org/classifiers/
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "Development Status :: 3 - Alpha",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-    ],
     # $ setup.py publish support.
     cmdclass={
         "upload": UploadCommand,
         "testupload": TestUploadCommand,
         "testinstall": TestInstallCommand,
-    },
-    entry_points={
-        "console_scripts": [
-            "deephyper=deephyper.core.cli._cli:main",
-            "deephyper-analytics=deephyper.core.analytics._analytics:main",
-        ]
-    },
+    }
 )
