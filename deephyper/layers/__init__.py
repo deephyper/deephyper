5191431552e6da49021f8fcb9926cd30ade13247
Shengli Jiang <44488777+sjiang87@users.noreply.github.com>
2021-11-23 02:07:10 -0600

    mpnn code modification (#117)

--- deephyper/layers/__init__.py
+++ deephyper/layers/__init__.py
@@ -3,3 +3,13 @@ from deephyper.layers.padding import Padding
 # When loading models with: "model.load('file.h5', custom_objects=custom_objects)"
 custom_objects = {"Padding": Padding}
 
+from deephyper.layers._mpnn import get_gcn_attention, get_mol_feature, get_all_mol_feat, \
+    SPARSE_MPNN, MP_layer, Message_Passer_NNM, Update_Func_MLP, Update_Func_GRU, Attention_GAT, Attention_SYM_GAT, \
+    Attention_GCN, Attention_Linear, Attention_Const, Attention_COS, Attention_Gen_Linear, GlobalAttentionPool, \
+    GlobalAttentionSumPool, GlobalSumPool, GlobalMaxPool, GlobalAvgPool
+
+__all__ = ["get_all_mol_feat", "get_mol_feature", "get_gcn_attention", "SPARSE_MPNN", "MP_layer", "Message_Passer_NNM",
+           "Update_Func_GRU", "Update_Func_MLP", "Attention_COS", "Attention_Linear", "Attention_Const",
+           "Attention_GAT", "Attention_GCN", "Attention_SYM_GAT", "Attention_Gen_Linear", "GlobalAttentionPool",
+           "GlobalAttentionSumPool", "GlobalSumPool", "GlobalAvgPool", "GlobalMaxPool"]
+
---
+++ docs/api/deephyper.layers._mpnn.rst
@@ -0,0 +1,13 @@
+deephyper.layers._mpnn
+==================
+
+.. toctree::
+   :maxdepth: 4
+
+
+.. automodule:: deephyper.layers._mpnn
+   :members:
+   :undoc-members: keras.engine.base_layer.Layer
+   :show-inheritance:
+   :inherited-members: keras.engine.base_layer.Layer
+
