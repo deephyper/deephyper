b6ee18947afe25cee31be51e6465f40b4d769f11
Deathn0t <romainegele@gmail.com>
2022-01-12 18:00:07 +0100

    added "queued" decorator for Evaluator classes

--- deephyper/evaluator/__init__.py
+++ deephyper/evaluator/__init__.py
@@ -9,12 +9,14 @@ from deephyper.evaluator._ray import RayEvaluator
 from deephyper.evaluator._serial import SerialEvaluator
 from deephyper.evaluator._subprocess import SubprocessEvaluator
 from deephyper.evaluator._thread_pool import ThreadPoolEvaluator
+from deephyper.evaluator._queue import queued
 
 __all__ = [
     "Evaluator",
     "EVALUATORS",
     "Job",
     "ProcessPoolEvaluator",
+    "queued",
     "RayEvaluator",
     "SerialEvaluator",
     "SubprocessEvaluator",
