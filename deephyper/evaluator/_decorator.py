54bfa5be4fd0844eac6abdaf7ca452883f8d705d
Deathn0t <romainegele@gmail.com>
2022-03-03 09:46:26 +0100

    making profile decorator pickable

--- deephyper/evaluator/_decorator.py
+++ deephyper/evaluator/_decorator.py
@@ -1,7 +1,12 @@
 import time
+from functools import wraps
 
+#! info [why is it important to use "wraps"]
+#! http://gael-varoquaux.info/programming/decoration-in-python-done-right-decorating-and-pickling.html
 
 def profile(run_function):
+
+    @wraps(run_function)
     def wrapper(*args, **kwargs):
         timestamp_start = time.time()
         objective = run_function(*args, **kwargs)
