c54c451a9357c7c027b753286f5b998ffebcd22f
Deathn0t <romainegele@gmail.com>
2022-06-06 12:18:11 +0200

    update notebook detection for colab

--- deephyper/evaluator/_evaluator.py
+++ deephyper/evaluator/_evaluator.py
@@ -31,14 +31,22 @@ def _test_ipython_interpretor() -> bool:
 
     Suggested by: https://stackoverflow.com/questions/15411967/how-can-i-check-if-code-is-executed-in-the-ipython-notebook
     """
+
+    # names of shells/modules using jupyter
+    notebooks_shells = ["ZMQInteractiveShell"]
+    notebooks_modules = ["google.colab._shell"]
+
     try:
-        shell = get_ipython().__class__.__name__
-        if shell == "ZMQInteractiveShell":
+        shell_name = get_ipython().__class__.__name__
+        shell_module = get_ipython().__class__.__module__
+
+        if shell_name in notebooks_shells or shell_module in notebooks_modules:
             return True  # Jupyter notebook or qtconsole
-        elif shell == "TerminalInteractiveShell":
+        elif shell_name == "TerminalInteractiveShell":
             return False  # Terminal running IPython
         else:
             return False  # Other type (?)
+
     except NameError:
         return False  # Probably standard Python interpreter
 
