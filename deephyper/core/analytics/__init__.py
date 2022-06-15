c7faec0dcda1350da343396763b1e077f00b7373
Deathn0t <romainegele@gmail.com>
2022-06-15 20:07:38 +0200

    adding scatter plot for single experiment visualization in dashboard

--- deephyper/core/analytics/__init__.py
+++ deephyper/core/analytics/__init__.py
@@ -1,9 +1,9 @@
 # for the documentation
 from . import _topk, _quick_plot, _dashboard
-from ._db_manager import DBManager
+from ._db_manager import DBManager, Query
 
 commands = [_topk, _quick_plot, _dashboard]
-__all__ = ["DBManager"]
+__all__ = ["DBManager", "Query"]
 
 __doc__ = "Provides command lines tools to visualize results from DeepHyper.\n\n"
 
