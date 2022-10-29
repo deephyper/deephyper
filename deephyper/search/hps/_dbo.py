0f69382ee9cd1f2dd281d1d66bc3edc14c2d6ef8
Deathn0t <romainegele@gmail.com>
2022-10-29 03:58:43 +0000

    added truncated exp distribution

--- deephyper/search/hps/_dbo.py
+++ deephyper/search/hps/_dbo.py
@@ -1,6 +1,6 @@
 import logging
 
-
+import scipy.stats
 import numpy as np
 from deephyper.search.hps._cbo import CBO
 from deephyper.evaluator import Evaluator, SerialEvaluator
@@ -88,11 +88,17 @@ class DBO(CBO):
                 acq_optimizer = "boltzmann_sampling"
 
         if acq_func[0] == "q":
-            kappa = random_state.exponential(kappa, size=self._evaluator.size)[
-                self._evaluator.rank
+            kappa = scipy.stats.truncexpon.rvs(kappa, size=self._evaluator.size)[
+                    self._evaluator.rank
             ]
-            xi = random_state.exponential(xi, size=self._evaluator.size)[
-                self._evaluator.rank
+            xi = scipy.stats.truncexpon.rvs(xi, size=self._evaluator.size)[
+                    self._evaluator.rank
+            ]
+            #kappa = random_state.exponential(kappa, size=self._evaluator.size)[
+            #    self._evaluator.rank
+            #]
+            #xi = random_state.exponential(xi, size=self._evaluator.size)[
+            #    self._evaluator.rank
             ]
             acq_func = acq_func[1:]
 
