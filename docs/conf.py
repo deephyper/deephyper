9538107d3270f3e0dd0bfb107a47a3da990c1a12
Deathn0t <romainegele@gmail.com>
2023-03-03 13:20:13 +0100

    adding jax as mock imports in doc

--- docs/conf.py
+++ docs/conf.py
@@ -269,7 +269,8 @@ todo_include_todos = True
 
 # makes sphinx do a mock import of mpi4py so it’s not broken when you try to do auto-docs and import mpi4py
 autodoc_mock_imports = [
-    "horovod" "jax",
+    "horovod",
+    "jax",
     "joblib",
     "matplotlib",
     "mpi4py",
