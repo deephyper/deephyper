f36e7c94e2af749db070010b4103676fb9574af5
Deathn0t <romainegele@gmail.com>
2023-06-28 19:02:42 +0200

    fix tutorial build

--- docs/conf.py
+++ docs/conf.py
@@ -62,7 +62,7 @@ def pull_tutorials(github_link, dest_dir, tutorial_branch):
     os.system(f"rm -rf {dest_dir}/.git")
 
 
-# pull_tutorials(tutorials_github_link, tutorials_dest_dir, tutorial_branch)
+pull_tutorials(tutorials_github_link, tutorials_dest_dir, tutorial_branch)
 
 # -- General configuration ---------------------------------------------------
 
