304416d6cbaaf84ec005d74a2c7b71b3c9ea7a81
Deathn0t <romainegele@gmail.com>
2023-09-21 08:49:58 +0200

    updated utility functions

--- deephyper/analysis/hpo.py
+++ deephyper/analysis/hpo.py
@@ -1,36 +1,49 @@
 """Visualization tools for Hyperparameter Optimization.
 """
+from typing import Tuple
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def filter_failed_objectives(df: pd.DataFrame) -> pd.DataFrame:
+def filter_failed_objectives(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """Filter out lines from the DataFrame with failed objectives.
 
     Args:
         df (pd.DataFrame): the results of a Hyperparameter Search.
+
     Returns:
-        pd.DataFrame: the results of a Hyperparameter Search without failed objectives.
+        Tuple[pd.DataFrame, pd.DataFrame]: ``df_without_failures, df_with_failures`` the first are results of a Hyperparameter Search without failed objectives and the second are results of Hyperparameter search with failed objectives.
     """
     if pd.api.types.is_string_dtype(df.objective):
-        df = df[~df.objective.str.startswith("F")]
-        df.objective = df.objective.astype(float)
-    return df
+        # Single-Objective
+        mask = df.objective.str.startswith("F")
+
+        df_with_failures = df[mask]
+
+        df_without_failures = df[~mask]
+        df_without_failures.objective = df_without_failures.objective.astype(float)
+
+    return df_without_failures, df_with_failures
 
 
-def parameters_at_max(df: pd.DataFrame) -> dict:
+def parameters_at_max(
+    df: pd.DataFrame, column: str = "objective"
+) -> Tuple[dict, float]:
     """Return the parameters at the maximum of the objective function.
 
     Args:
         df (pd.DataFrame): the results of a Hyperparameter Search.
+        column (str, optional): the column to use for the maximization. Defaults to ``"objective"``.
 
     Returns:
-        dict: the parameters at the maximum of the objective function.
+        Tuple[dict, float]: the parameters at the maximum of the ``column`` and its corresponding value.
     """
-    df = filter_failed_objectives(df)
-    config = df.iloc[df.objective.argmax()].to_dict()
+    df, _ = filter_failed_objectives(df)
+    idx = df[column].argmax()
+    value = df.iloc[idx][column]
+    config = df.iloc[idx].to_dict()
     config = {k[2:]: v for k, v in config.items() if k.startswith("p:")}
-    return config
+    return config, value
 
 
 def plot_search_trajectory_single_objective_hpo(results, ax=None, **kwargs):
