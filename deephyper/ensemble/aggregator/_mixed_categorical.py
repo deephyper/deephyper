d05ea0772ab7e0c0a37735b82ec5084a4a48b0a5
Deathn0t <romainegele@gmail.com>
2024-08-16 14:57:14 +0200

    implemented decomposition of uncertainty for classification with aleatoric and epistemic decomposition

--- deephyper/ensemble/aggregator/_mixed_categorical.py
+++ deephyper/ensemble/aggregator/_mixed_categorical.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.stats as ss
 
 from deephyper.ensemble.aggregator._aggregator import Aggregator
 
@@ -12,7 +13,11 @@ class MixedCategoricalAggregator(Aggregator):
         decomposed_uncertainty (bool, optional): If ``True``, the uncertainty of the ensemble is decomposed into aleatoric and epistemic components. Default is ``False``.
     """
 
-    def __init__(self, decomposed_uncertainty: bool = False):
+    def __init__(
+        self, uncertainty_method="confidence", decomposed_uncertainty: bool = False
+    ):
+        assert uncertainty_method in ["confidence", "entropy"]
+        self.uncertainty_method = uncertainty_method
         self.decomposed_uncertainty = decomposed_uncertainty
 
     def aggregate(self, y):
@@ -27,36 +32,37 @@ class MixedCategoricalAggregator(Aggregator):
         # Categorical probabilities (n_predictors, n_samples, ..., n_classes)
         y_proba_models = np.asarray(y)
         y_proba_ensemble = np.mean(y_proba_models, axis=0)
-        # n_classes = y_proba_models.shape[-1] # For normalization with number of classes
-        n_classes = 1
+        n_classes = y_proba_models.shape[-1]  # For normalization with number of classes
+        # n_classes = 1
 
         agg = {
             "loc": y_proba_ensemble,
         }
 
-        if self.decomposed_uncertainty:
-            # Mode of the ensemble (n_samples, ...)
-            y_mode_ensemble = np.argmax(y_proba_ensemble, axis=-1)  # (n_samples,)
-
-            # Uncertainty of the mode: 1 - confidence(n_predictors, n_samples, ...)
-            y_aleatoric = (
-                np.mean(1 - np.max(y_proba_models, axis=-1), axis=0) * n_classes
-            )
-            y_epistemic = (
-                np.mean(
-                    1
-                    - np.take_along_axis(
-                        y_proba_models,  # (n_predictors, n_samples, n_classes)
-                        y_mode_ensemble.reshape(1, y_mode_ensemble.shape[0], 1),
-                        axis=-1,
-                    ),
-                    axis=0,
+        # Confidence of the ensemble: max probability of the ensemble
+        if self.uncertainty_method == "confidence":
+            uncertainty = (1 - np.max(y_proba_ensemble, axis=-1)) * n_classes
+            if not self.decomposed_uncertainty:
+                agg["uncertainty"] = uncertainty
+            else:
+                # Uncertainty of the mode: 1 - confidence(n_predictors, n_samples, ...)
+                y_aleatoric = (
+                    np.mean(1 - np.max(y_proba_models, axis=-1), axis=0) * n_classes
                 )
-                * n_classes
-            )
-            agg["uncertainty_aleatoric"] = y_aleatoric
-            agg["uncertainty_epistemic"] = y_epistemic
-        else:
-            agg["uncertainty"] = (1 - np.max(y_proba_ensemble, axis=-1)) * n_classes
+                y_epistemic = uncertainty - y_aleatoric
+
+                agg["uncertainty_aleatoric"] = y_aleatoric
+                agg["uncertainty_epistemic"] = y_epistemic
+
+        # Entropy of the ensemble
+        elif self.uncertainty_method == "entropy":
+            uncertainty = ss.entropy(y_proba_ensemble, axis=-1)
+            if not self.decomposed_uncertainty:
+                agg["uncertainty"] = uncertainty
+            else:
+                # Expectation over predictors in the ensemble
+                expected_entropy = np.mean(ss.entropy(y_proba_models, axis=-1), axis=0)
+                agg["uncertainty_aleatoric"] = expected_entropy
+                agg["uncertainty_epistemic"] = uncertainty - expected_entropy
 
         return agg
