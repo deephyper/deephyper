132cad400da29e349dbf173731acc9eab3ad126c
Deathn0t <romainegele@gmail.com>
2024-09-09 14:53:51 +0200

    updating doc

--- deephyper/__init__.py
+++ deephyper/__init__.py
@@ -5,6 +5,7 @@ DeepHyper's software architecture is designed to be modular and extensible. It i
 * :mod:`deephyper.ensemble`: Tools to build ensembles of neural networks with uncertainty quantification.
 * :mod:`deephyper.evaluator` : Tools to distribute the evaluation of tasks (e.g., neural network trainings).
 * :mod:`deephyper.hpo`: Tools to define and run hyperparameter optimization (HPO) and neural architecture search (NAS).
+* :mod:`deephyper.predictor`: Tools to wrap pure predictive models (i.e., that can only predict).
 * :mod:`deephyper.stopper`: Tools to define multi-fidelity strategies for hyperparameter optimization (HPO) and neural architecture search (NAS).
 
 
