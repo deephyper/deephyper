4ba8d11f2604a905001ab7eb09c95d0991f8ae67
Deathn0t <romainegele@gmail.com>
2024-09-13 11:18:15 +0200

    adding online selector for ensemble with hpo

--- deephyper/ensemble/selector/__init__.py
+++ deephyper/ensemble/selector/__init__.py
@@ -1,8 +1,9 @@
 """Subpackage providing methods that select a subset of predictors from a set of available predictors in order to build an ensemble."""
 
+from deephyper.ensemble.selector._greedy import GreedySelector
+from deephyper.ensemble.selector._online_selector import OnlineSelector
 from deephyper.ensemble.selector._selector import Selector
 from deephyper.ensemble.selector._topk import TopKSelector
-from deephyper.ensemble.selector._greedy import GreedySelector
 
 
-__all__ = ["Selector", "TopKSelector", "GreedySelector"]
+__all__ = ["GreedySelector", "OnlineSelector", "Selector", "TopKSelector"]
