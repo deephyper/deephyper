9a1aa200e36ea02859c81cf0d59df5a0af043920
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2024-12-02 16:02:23 -0500

    Properly define the version number

--- deephyper/__version__.py
+++ deephyper/__version__.py
@@ -1,6 +1,6 @@
-VERSION = (0, 8, 1)
+"""Define version number for the package."""
 
-__version__ = ".".join(map(str, VERSION))
+__version__ = "0.8.1"
 
 # alpha/beta/rc tags
 __version_suffix__ = ""
