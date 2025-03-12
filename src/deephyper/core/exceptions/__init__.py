9aea5dc2b297c3cf7e4497426e9901e345a7bd3c
Deathn0t <romainegele@gmail.com>
2025-03-12 09:06:08 +0100

    removing unused exceptions

--- src/deephyper/core/exceptions/__init__.py
+++ src/deephyper/core/exceptions/__init__.py
@@ -1,17 +1,5 @@
 """Set of root exceptions for the package."""
 
-
-class DeephyperError(Exception):
-    """Root deephyper exception."""
-
-
-class DeephyperRuntimeError(RuntimeError):
-    """Raised when for error that doesn't fall in other categories.
-
-    The associated value is a string indicating what precisely went wrong.
-    """
-
-
 class SearchTerminationError(RuntimeError):
     """Raised when a search is terminated."""
 
@@ -23,12 +11,3 @@ class MaximumJobsSpawnReached(SearchTerminationError):
 class TimeoutReached(SearchTerminationError):
     """Raised when the timeout of the search was reached."""
 
-
-class RunFunctionError(RuntimeError):
-    """Raised when error occurs in run-function."""
-
-    def __init__(self, msg: str = None) -> None:
-        self.msg = msg
-
-    def __str__(self) -> str:  # noqa: D105
-        return self.msg
